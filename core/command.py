82dae1463c38b8422ae1db37b3d0eca0eb3a60df
James Percent <james@syndeticlogic.org>
2013-07-09 15:15:43 -0400

    Added serialization of commands

--- core/command.py
+++ core/command.py
@@ -1,20 +1,40 @@
-from unlock.core.util import misc
 import socket
+import cPickle
+import json
+from unlock.util import DatagramWrapper
 
-class BaseCommand(object):
-    def __init__(self, delta, decision, selection, data):
+class Command(object):
+    def __init__(self, delta=None, decision=None, selection=None, data=None, json=False):
         self.delta = delta
         self.decision = decision
         self.selection = selection
         self.data = data
-        
-        
+        self.json = json
+
+    @staticmethod
+    def serialize(command):
+        if command.json:
+            ret = json.dumps(command)
+        else:
+            ret = cPickle.dumps(command)
+        return ret
+
+    @staticmethod
+    def deserialize(serialized_command, json=False):
+        if json:
+            ret = json.loads(serialized_command)
+        else:
+            ret = cPickle.loads(serialized_command)
+        return ret
+            
+          
 class CommandReceiverInterface(object):
     def next_command(self):
         raise NotImplementedError("Every CommandReceiverInterface must implement the next_command method")
     
     def stop(self):
         raise NotImplementedError("Every CommandReceiverInterface must implement the stop method")
+        
         
 class CommandSenderInterface(object):
     def send(self, command):
@@ -26,15 +46,15 @@ class DatagramCommandSender(object):
         pass
         
         
-class DatagramCommandReceiver(CommandReceiverInterface):
+class DatagramDecomposedCommandReceiver(CommandReceiverInterface):
     def __init__(self, address='127.0.0.1', decision_port=33445, selection_port=33446, data_port=33447, socket_timeout=0.001):
         self.decision_socket = DatagramWrapper(address, decision_port, socket_timeout)
         self.selection_socket = DatagramWrapper(address, selection_port, socket_timeout)
         self.data_socket = DatagramWrapper(address, data_port, socket_timeout)
         
     def next_command(self, delta_since_last_poll):
-        decision = self.decision_socket.receive_from(int)
-        selection = self.selection_socket.receive_from(int)
+        decision = self.decision_socket.receive(int)
+        selection = self.selection_socket.receive(int)
         data = []
         done = False
         while not done:
@@ -52,9 +72,15 @@ class DatagramCommandReceiver(CommandReceiverInterface):
 class InlineCommandReceiver(CommandReceiverInterface):
     def __init__(self):
         self.Q = []
+        self.pos = 0
     
     def next_command(self):
-        return self.Q[-1]
+        if self.pos == len(self.Q):
+            ret = None
+        else:
+            ret = self.Q[self.pos]
+            self.pos += 1
+        return ret
     
     def stop(self):
         logger.debug("stop called")
--- core/tests/test_command.py
+++ core/tests/test_command.py
@@ -3,14 +3,19 @@ import unittest
 
 class CommandTests(unittest.TestCase):
     """Test suite for the command module"""
-    def testBaseCommand(self):
-        bc = BaseCommand(-1, -2, -3, [-1])
+    def testCommand(self):
+        bc = Command(-1, -2, -3, [-1])
         self.assertEquals(-1, bc.delta)
         self.assertEquals(-2, bc.decision)
         self.assertEquals(-3, bc.selection)
         self.assertEquals([-1], bc.data)
-            
-            
+        binary_bc = Command.serialize(bc)
+        bc1 = Command.deserialize(binary_bc)
+        self.assertEquals(bc.delta, bc1.delta)
+        self.assertEquals(bc.decision, bc1.decision)
+        self.assertEquals(bc.selection, bc1.selection)        
+        self.assertEquals(bc.data, bc1.data)
+        
     def testCommandReceiverInterface(self):
         pass
             
@@ -21,10 +26,10 @@ class CommandTests(unittest.TestCase):
         icr = InlineCommandReceiver()
         command = icr.next_command()
         self.assertEquals(None, command)
-        bc = BaseCommand(-1, -2, -3, [-1])
-        bc1 = BaseCommand(0, 1, 2, [0])
-        bc2 = BaseCommand(1, 2, 3, [1])
-        bc3 = BaseCommand(2, 3, 4, [5])
+        bc = Command(-1, -2, -3, [-1])
+        bc1 = Command(0, 1, 2, [0])
+        bc2 = Command(1, 2, 3, [1])
+        bc3 = Command(2, 3, 4, [5])
         icr.put(bc)
         icr.put(bc1)
         icr.put(bc2)
