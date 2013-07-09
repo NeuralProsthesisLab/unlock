ccd62ab10c67321b0b549686aa85b0c1017c4205
James Percent <james@syndeticlogic.org>
2013-07-08 20:07:32 -0400

    Added logging config to the unlock package

--- __init__.py
+++ __init__.py
@@ -1,3 +1,21 @@
+import os
+import json
+import logging.config
+
+def setup_logging(config='logging.json', log_level=logging.INFO, env_key='LOG_CFG'):
+    """Setup logging configuration """
+    value = os.getenv(env_key, None)
+    if value:
+        config = value
+        
+    if os.path.exists(config):
+        with open(path, 'rt') as f:
+            config = json.load(f.read())
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=log_level)
+
 from apps import *
 from core import *
 from core.util import *
+
