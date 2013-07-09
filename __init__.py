90afa7daaf43352f6a0d92721b6325f0e925a0e3
James Percent <james@syndeticlogic.org>
2013-07-09 12:40:22 -0400

    Fixed logging setup.  Will use logging.json in the package directory

--- __init__.py
+++ __init__.py
@@ -1,21 +1,28 @@
 import os
 import json
+import inspect
 import logging.config
 
-def setup_logging(config='logging.json', log_level=logging.INFO, env_key='LOG_CFG'):
+def setup_logging(config=None, log_level=logging.INFO, env_key='LOG_CFG'):
     """Setup logging configuration """
-    value = os.getenv(env_key, None)
+    if not config:
+        config=os.path.join(os.path.dirname(inspect.getfile(setup_logging)), 'logging.json')
+        
+    value = os.getenv(env_key, None)        
     if value:
         config = value
         
     if os.path.exists(config):
-        with open(path, 'rt') as f:
-            config = json.load(f.read())
-        logging.config.dictConfig(config)
+        with open(config, 'rt') as file_descriptor:
+            json_binary = file_descriptor.read()
+            json_string = json.loads(json_binary)
+        logging.config.dictConfig(json_string)
     else:
         logging.basicConfig(level=log_level)
+        
+setup_logging()
 
 from apps import *
 from core import *
-from core.util import *
+from util import *
 
