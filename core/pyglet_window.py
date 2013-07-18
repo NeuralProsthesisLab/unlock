01ebe4fd7cb03ab66491c2b5cbd3e69ef1384ba4
James Percent <james@syndeticlogic.org>
2013-07-18 19:11:26 -0400

    Moved modules command, pyglet_window, and timed_stimuli.
    Command.py is the module that encapulates all that base code
    to  manage the BCI commands that are passed to controllers.
    
    Pyglet_window is the base package that manages the pyglet
    runtime.
    
    Timed_stimuli contains logic to manage emissions of values
    based on time.  Such sequences can be used to create an
    SSVEP M-Sequence display.

---
+++ core/pyglet_window.py
@@ -0,0 +1,69 @@
+from unlock.core import PygletKeyboardCommand
+import pyglet
+import time
+
+
+class ScreenDescriptor(object):
+    def __init__(self, batch, width, height):
+        self.batch = batch
+        self.width = width
+        self.height = height
+        
+    @staticmethod
+    def create(width, height):
+        batch = pyglet.graphics.Batch()
+        return ScreenDescriptor(batch, width, height)
+            
+            
+class PygletController(pyglet.window.Window):
+    def __init__(self, fullscreen=False, vsync=False, show_fps=False):
+        super(PygletWindow, self).__init__(**{'fullscreen':fullscreen, 'vsync':vsync})
+        self.views = []
+        if show_fps:
+            self.fps = pyglet.clock.ClockDisplay().draw
+        else:
+            def empty():
+                pass
+            self.fps = empty
+        self.count = 0
+        
+        @self.event
+        def on_draw():
+            self.count += 1
+            print "Draw... ", time.time()
+            if self.count == 80:
+                pyglet.app.exit()
+                
+            self.clear()
+            for view in self.views:
+                view.render() #Draws the apps to the screen
+            self.fps()
+                
+        @self.event
+        def on_key_press(symbol, modifiers):
+            command = PygletKeyboardCommand(symbol, modifiers)
+            if command.stop:
+                stop = self.app.quit()
+                if stop:
+                    pyglet.app.exit()
+            if command.decision or command.selection:
+                self.app.keyboard_input(command)
+                
+    def set_app(self, app):
+        self.app = app
+        pyglet.clock.schedule_interval(app.poll_bci, 1.0/120.0)
+        
+    def add_view(self, view):
+        self.views.append(view)
+            
+    def start(self):
+        pyglet.app.run()
+            
+            
+class UnlockController(PygletController):
+    def __init__(self):
+        pass
+    def someshisit(self):
+        pass
+        
+       
---
+++ core/tests/test_context.py
@@ -0,0 +1,30 @@
+
+from .. import ApplicationContext, XMLConfig
+
+import time
+import unittest
+import unlock
+import inspect
+import os
+
+class ContextTests(unittest.TestCase):
+        
+    def testApplicationContext(self):
+        app_ctx_xml = os.path.join(os.path.dirname(inspect.getfile(ContextTests)), "app-ctx.xml")
+        app_ctx = ApplicationContext(XMLConfig(config_location = app_ctx_xml))
+        trial_state = app_ctx.get_object("trial_state")
+        trial_state.start()
+        start = time.time()
+        state, change = trial_state.update_state(0)
+        self.assertEquals(unlock.util.RunState.running, state)
+        self.assertEquals(unlock.util.TrialState.unchanged, change)
+        self.assertEquals(1.0, trial_state.time_state.trial_duration)
+        self.assertEquals(1.0, trial_state.time_state.rest_duration)        
+            
+            
+def getSuite():
+    return unittest.makeSuite(ContextTests,'test')
+
+if __name__ == "__main__":
+    unittest.main()
+    
