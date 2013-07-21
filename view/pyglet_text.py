53060475863dd72847a95ab20e1c6431eef61db1
James Percent <james@syndeticlogic.org>
2013-07-21 18:41:48 -0400

    Added BellRingTextLabelDecorator and fixed PygletTextLabel

--- view/pyglet_text.py
+++ view/pyglet_text.py
@@ -1,3 +1,8 @@
+from view import UnlockView
+
+import pyglet
+import inspect
+import os
 
 class PygletTextLabel(UnlockView):
     def __init__(self, model, screen, text, x, y, anchor_x='center', anchor_y='center', font='Helvetica', size=48,
@@ -24,15 +29,43 @@ class PygletTextLabel(UnlockView):
             color = color + (255,)
         self.color = color
         self.label = pyglet.text.Label(self.text, font_name=self.font, font_size=self.size,
-                                       x=self.screen.x+self.x, y=self.screen.y+self.y,
+                                    x=self.screen.x+self.x, y=self.screen.y+self.y,
                                     anchor_x=anchor_x, anchor_y=anchor_y, color=self.color,
-            #group=group
-            batch=self.screen.batch)
-    
+                                    group=group,batch=self.screen.batch)
+        self.label.text = text
+        
     def render(self):
-        self.label.visible = self.model.get_model()
+        if self.model.get_state():
+            self.label.text = self.text
+        else:
+            self.label.text = ''
+            
+        self.screen.batch.draw()
         
+
+class BellRingTextLabelDecorator(UnlockView):
+    def __init__(self, text_label):
+        self.text_label = text_label
+        self.model = self.text_label.model
+        self.sound = pyglet.media.StaticSource(pyglet.media.load(os.path.join(os.path.dirname(inspect.getfile(BellRingTextLabelDecorator)), 'bell-ring-01.mp3')))
+        self.state = True
         
+    def render(self):
+        self.last_state = self.state
+        self.state = self.model.get_state()
+        if self.last_state != self.state:
+            self.sound.play()
+            
+        if self.state:
+            self.text_label.label.text = self.text_label.text
+        else:
+            self.text_label.label.text = ''
+            
+        self.text_label.screen.batch.draw()
+   
+
+
+
 #class Image(object):
 #    def __init__(self, window, filename, anchor_x, anchor_y, position_x, position_y):
 #        self.window = window
