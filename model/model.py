42a81b4d7f59be75d3252341ebcb7c843561e9ea
James Percent <james@syndeticlogic.org>
2013-07-21 19:35:36 -0400

    Added UnlockModel and AlternatingBinaryStateModel

---
+++ model/model.py
@@ -0,0 +1,25 @@
+
+class UnlockModel(object):
+    def __init__(self):
+        super(UnlockModel, self).__init__()
+        self.state = None
+        
+    def get_state(self):
+        return self.state
+    
+    
+class AlternatingBinaryStateModel(UnlockModel):
+    def __init__(self, hold_duration=300):
+        self.hold_duration = hold_duration
+        self.state = True
+        self.count = 0
+        
+    def get_state(self):
+        ret = self.state
+        self.count += 1
+        if self.count % self.hold_duration == 0:
+            self.state = not self.state
+        return ret
+            
+           
+    
