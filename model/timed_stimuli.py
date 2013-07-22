cceee70ace142bcaf778f878e714882016d563fc
James Percent <james@syndeticlogic.org>
2013-07-22 16:33:06 -0400

    Serveral fixes for TimedStimulus and TimedStimuli

--- model/timed_stimuli.py
+++ model/timed_stimuli.py
@@ -1,80 +1,69 @@
-import pyglet
-import array
-import socket
-import time
 
-from unlock.util import TrialState, Trigger
+from unlock.util import TrialState, TrialTimeState, RunState, Trigger, SequenceState
 from model import UnlockModel
 
+import logging
 
-class TimedSequenceStimuliManager(UnlockModel):
+
+class TimedStimuli(UnlockModel):
     """ Manages multiple timed, sequence-based stimuli. """
-    def __init__(self, state, trigger):
+    def __init__(self, state):
         self.state = state
         self.stimuli = set([])
-            
+        self.logger = logging.getLogger(__name__)
+        
     def add_stimulus(self, stimulus):
-        """
-        add_stimulus(): adds a stimulus to the managed set
-        """        
         self.stimuli.add(stimulus)
             
     def start(self):
-        """
-        start(): starts the stimuli
-        """
-        state.start()
+        self.state.start()
         for stimulus in self.stimuli:
             stimulus.start()
             
     def pause(self):
-        """
-        pause(): pause the stimuli
-        """
         for stimulus in self.stimuli:
             stimulus.stop()
             
     def stop(self):
-        """
-        stop(): stops the trial
-        """
         self.state.stop()
         for stimulus in self.stimuli:
             stimulus.stop()
             
     def process_command(self, command):
-        """
-        process_command(): processes the next command
-        """
-        if self.state.stopped():
-            log.debug("TimedSequenceStimuliManager.process_command: called when stopped")
-            return
+        if self.state.is_stopped():
+           self.logger.debug("TimedSequenceStimuliManager.process_command: called when stopped")
+           return
             
         if len(self.stimuli) < 1:
-            log.debug("TimedSequenceStimuliManager.process_command: no stimulus available ")
+            self.logger.debug("TimedSequenceStimuliManager.process_command: no stimulus available ")
             return
             
         ret = Trigger.Null
         state, change_value = self.state.update_state(command.delta)
+        self.logger.debug("state, change value ", state, change_value)
         if state == RunState.running:
             sequence_start_trigger = False
             for stimulus in self.stimuli:
-                sequence_start_trigger = stimulus.update(command)
+                sequence_start_trigger = stimulus.process_command(command)
             if sequence_start_trigger:
                 ret = Trigger.Start
-        elif change_value == TrialState.trial_expiry:
-            self.pause()
         elif change_value == TrialState.rest_expiry:
             self.start()
+        elif change_value == TrialState.trial_expiry:
+            self.pause()
             
         return ret
     
+    def get_state(self):
+        raise NotImplementedError()
+    
     @staticmethod
-    def create(trial_state):
-        state = TrialState()
+    def create(stimuli_duration):
+        trial_state = TrialState.create(stimuli_duration, 0)
+        return TimedStimuli(trial_state)
             
             
-class TimedSequenceStimulus(UnlockModel):
+class TimedStimulus(UnlockModel):
     """
     Emits a sequence of values at fixed time interval.
     time_state: manages the time (when to emit the next value)
@@ -83,46 +72,45 @@ class TimedSequenceStimulus(UnlockModel):
     def __init__(self, time_state, seq_state):
         self.time_state = time_state
         self.seq_state = seq_state
-        self.state_value = False
+        self.state = False
+        self.logger = logging.getLogger(__name__)
             
     def get_state(self):
-        return state_value
+        return self.state
             
     def start(self):
         self.seq_state.start()
-        self.trial_time.begin_trial()
+        self.time_state.begin_trial()
         self.first_pass = True
             
     def stop(self):
-        """
-        Stops stimulus 
-        """
-        self.state_value = False
+        self.state = False
             
     def process_command(self, command):
         """
         Updates the stimulus to the next state in the presentation
         sequence if the trial time is exceeded.
         
-        A value of True is returned at the start of the sequence.
+        A value of Trigger.Start is returned at the start of the sequence.
         """
-        start_trigger = False
+        start_trigger = Trigger.Null
         self.time_state.update_trial_time(command.delta)
         if self.time_state.is_trial_complete() or self.first_pass:
             self.first_pass = False
             self.state = self.seq_state.state()
+            self.logger.debug("TimedStimulus trial complete; next state = ", self.state)
             if self.seq_state.is_start():
-                start_trigger = True
+                start_trigger = Trigger.Start
             self.time_state.begin_trial()
             self.seq_state.step()
-                
+            
         return start_trigger
             
     @staticmethod
-    def create(self, rate, sequence=(1,0), value_transformer_fn=lambda x: bool(x)):
+    def create(rate, sequence=(1,0), value_transformer_fn=lambda x: bool(x)):
         flick_rate = 0.5/rate
         time_state = TrialTimeState(flick_rate, 0)
         seq_state = SequenceState(sequence, value_transformer_fn)
-        return TimedSequenceStimulus(flick_rate, time_state, seq_state)
+        return TimedStimulus(time_state, seq_state)
             
          
