069fb2f9f3db789f8f5b70edb7c5d4b2275a8ae1
James Percent <james@syndeticlogic.org>
2013-07-22 13:14:21 -0400

    Modified CueState to use the enum defined in Trigger rather than reproduce its own.  Also, modified the transition function to return the Trigger

--- model/cue_state.py
+++ model/cue_state.py
@@ -14,46 +14,41 @@ class RandomCueStateMachine(UnlockModel):
         next_state.last = self.state
         next_state.start()
         self.state = next_state
+        return self.state.trigger
         
-    def indicator_state(self):
-        self.__transition__(self.rest_state)
+    def indicate_state(self):
+        return self.__transition__(self.rest_state)
         
     def rest_state(self):
-        self.__transition__(self.cue_states[self.rand.randint(0, len(self.cue_states)-1)])
+        return self.__transition__(self.cue_states[self.rand.randint(0, len(self.cue_states)-1)])
         
     def cue_state(self):
-        self.__transition__(self.indicator_state)
+        return self.__transition__(self.indicator_state)
         
     def process_command(self, command):
         self.count += 1
-        self.state = self.state.process_command(command)
+        return self.state.process_command(command)
         
     @staticmethod
     def create(trials, cues, cue_duration, rest_duration, indication_duration, seed=42):
         cue_states = []
         for cue in cues:
             cue_states.append(CueState.create(cue, cue_duration))
-        rest_state = CueState.create(CueState.Rest, rest_duration)
-        indicator_state = CueState.create(CueState.Indicator, indication_duration)
-        state_machine = RandomCueStateMachine(random.Random(seed), trials, cue_states, rest_state, indicator_state)
+        rest_state = CueState.create(Trigger.Rest, rest_duration)
+        indicate_state = CueState.create(Trigger.Indicate, indication_duration)
+        state_machine = RandomCueStateMachine(random.Random(seed), trials, cue_states, rest_state, indicate_state)
         for cue_state in cues_states:
             cue_state.transition_fn = state_machine.cue_state
         rest_state.transition_fn = state_machine.rest_state
-        indicator_state.transition_fn = state_machine.indicator_state
+        indicator_state.transition_fn = state_machine.indicate_state
         rest_state.start()
         
         
 class CueState(UnlockModel):
-    Rest = 0
-    Indicator = 1
-    Left = 2
-    Right = 3
-    Up = 4
-    Down = 5
-    def __init__(self, state_id, trial_time_state, transition_fn=None):
+    def __init__(self, trigger, trial_time_state, transition_fn=None):
         super(TimedState, self).__init__()
         self.state = False
-        self.state_id = state_id
+        self.trigger = trigger
         self.trial_time_state = trial_time_state
         self.transition_fn = transition_fn
         
@@ -70,9 +65,12 @@ class CueState(UnlockModel):
         
     def process_command(self, command):
         assert self.transition_fn != None
+        ret = Trigger.Null
         self.trial_state.update_trial_time(command.delta)
         if self.trial_state.is_trial_complete():
-            self.transition_fn()
+            ret = self.transition_fn()
+            
+        return ret
             
     @staticmethod
     def create(state_id, duration):
