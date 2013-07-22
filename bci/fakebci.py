70d742af34170a28d76c8c6ffc8d0015eec78939
James Percent <james@syndeticlogic.org>
2013-07-22 13:08:24 -0400

    Removed unused class in fakebci.py

--- bci/fakebci.py
+++ bci/fakebci.py
@@ -1,34 +1,38 @@
 import sys
 import random
-class BCIConfig(object):
-    pass
+
 
 class FakeBCI(object):
-    def __init__(self, bci_config):
+    def __init__(self):
         self.rand = random.Random(1337)
         self.getdata_count = 0
         self.acquire_count = 0
         self.ret = None
+        
     def open(self, port):
         self.port = port
         return 1
+        
     def init(self, analog_channels, digital_channels):
         self.init = True
         self.achan = analog_channels
         self.dchan = digital_channels
         return 1
+        
     def start(self):
         self.start = True
         return 1
+        
     def acquire(self):
         self.acquire_count += 1
         return 1
+        
     def getdata(self, chans):
         self.getdata_count += 1
         self.last_chans = chans
-#        if self.ret == None:
         self.ret = []        
         for i in range(chans):
             self.ret.append(self.rand.randint(1, sys.maxint))
-        #print 'FakeBCI.getdata ret = ', self.ret
         return self.ret
+        
+        
