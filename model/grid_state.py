43b0a32f87503d4680f2cdc502a027a199d37ecf
James Percent <james@syndeticlogic.org>
2013-07-23 14:24:49 -0400

    Fixed GridState

--- model/grid_state.py
+++ model/grid_state.py
@@ -5,6 +5,7 @@ class GridStateChange(object):
     XChange = 0
     YChange = 1
     def __init__(self, change, step_value=None):
+        super(GridStateChange, self).__init__()
         self.change = change
         self.step_value = step_value
         
@@ -14,8 +15,8 @@ class GridState(UnlockModel):
     DecrementYCursor=2
     DecrementXCursor=3
     IncrementXCursor=4
-    def __init__(self, controllers, center_x, center_y):
-        super(CrawlableGrid, self).__init__()
+    def __init__(self, controllers):
+        super(GridState, self).__init__()
         assert len(controllers) > 0
         
         self.ordering = [(0,1), (1,0), (0,-1), (-1,0), (1,1), (1,-1), (-1,-1), (-1,1)]
@@ -23,49 +24,49 @@ class GridState(UnlockModel):
         self.state_change = None
         self.controllers = {}
         
+        for slot in self.ordering:
+            self.controllers[slot] = 'deadbeef'
+        
         index = 0
         for controller in controllers:
             x_offset, y_offset = self.ordering[index]
             self.controllers[(x_offset, y_offset)] = controller
             index += 1
-            
-    def start(self):
-        pass
-        
-    def stop(self):
-        pass
-        
+          
     def process_command(self, command):
         if command.decision is not None:
-            self.process_decision(decision)
+            self.process_decision(command.decision)
             
-        if selection:
-            if self.cursor in self.controllers:
-                controller = self.controllers[self.cursor]
+        if command.selection:
+            if self.state in self.controllers:
+                controller = self.controllers[self.state]
                 controller.activate()
                 
     def process_decision(self, decision):
-        current_x, current_y = self.cursor
-            
-        if decision == CrawlableGrid.IncrementYCursor:
+        current_x, current_y = self.state
+
+        if decision == GridState.IncrementYCursor:
             new_state = (current_x, current_y+1)
             change = GridStateChange.YChange, 1
             
-        elif decision == CrawlableGrid.DecrementYCursor:
+        elif decision == GridState.DecrementYCursor:
             new_state = (current_x, current_y-1)
             change = GridStateChange.YChange, -1
             
-        elif decision == CrawlableGrid.DecrementXCursor:
+        elif decision == GridState.DecrementXCursor:
             new_state = (current_x-1, current_y)            
             change = GridStateChange.XChange, -1
             
-        elif decision == CrawlableGrid.IncrementXCursor:
+        elif decision == GridState.IncrementXCursor:
             new_state = (current_x+1, current_y)
             change = GridStateChange.XChange, 1
-            
+
+        #print "decision = ", decision, ' new state = ', new_state, ' controllers ', self.controllers
+        #print "state = ", self.state
         if new_state in self.controllers:
+            self.state = new_state
             self.state_change = GridStateChange(*change)
-            
+                    
     def get_state(self):
         ret = self.state_change
         self.state_change = None
