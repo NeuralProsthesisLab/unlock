cb3ada05e96c06fb1b44f44336caac72589a4b2c
James Percent <james@syndeticlogic.org>
2013-07-23 14:29:32 -0400

    Added a default center image to the GridView and fixed issues

--- view/grid.py
+++ view/grid.py
@@ -1,31 +1,52 @@
+
 from unlock.model import UnlockModel
+from unlock.model import GridStateChange
+from pyglet_sprite import PygletSprite
+from pyglet_text import PygletTextLabel
+from view import UnlockView
+
+import inspect
+import os
 
 
-class Grid(UnlockView):
-    #icons = (path, name)
+class GridView(UnlockView):
     def __init__(self, model, canvas, icons, center_x, center_y, rect_xoffset=64, rect_yoffset=64,
-                 icon_width=128, icon_length=128):
-        super(Grid, self).__init__()
+                 icon_width=128, icon_height=128):
+        super(GridView, self).__init__()
+
+        self.model = model
         self.icon_width = icon_width
-        self.icon_length = icon_length
-        
+        self.icon_height = icon_height
         self.controller_count = 0
         self.cursor = (0,0)
-        self.grid = {}
+        self.icon_widgets = []
         self.vertex_list = self.drawRect(center_x-rect_xoffset, center_y-rect_yoffset,
-                                            self.icon_width, self.icon_length)
+                                            icon_width, icon_height, canvas.batch)
         index = 0
-        model = UnlockModel(state=True)
+        fixed_state_model = UnlockModel(state=True)
+        name = "Unlock Dashboard"
+        path=os.path.join(os.path.dirname(inspect.getabsfile(GridView)), 'unlock.png')
+            
+        try:
+            unlock_widget = PygletSprite.create_image_sprite(fixed_state_model, canvas, path, center_x, center_y)
+        except AttributeError:
+            unlock_widget = PygletTextLabel(fixed_state_model, canvas, name,center_x, center_y, size=18)
+            
+        unlock_widget.render()
+        self.icon_widgets.append(unlock_widget)
+        
         for icon_path, icon_name in icons:
-            x_offset, y_offset = self.model.ordering[index]
-            icon_x = center_x + x_offset * self.icon_width
-            icon_y = center_y + y_offset * self.icon_length
+            x_offset, y_offset = model.ordering[index]
+            icon_x = center_x + x_offset * icon_width
+            icon_y = center_y + y_offset * icon_height
             try:
-                icon_widget = PygletSprite.create_image_sprite(model, canvas, icon_path, icon_x, icon_y)
+                icon_widget = PygletSprite.create_image_sprite(fixed_state_model, canvas, icon_path, icon_x, icon_y)
             except AttributeError:
-                icon_widget = self.screen.drawText(icon_name, icon_x, icon_y, size=18)
+                icon_widget = PygletTextLabel(fixed_state_model, canvas, icon_name, icon_x, icon_y, size=18)
+                
+            icon_widget.render()                    
             self.icon_widgets.append(icon_widget)
-            index += 0
+            index += 1
             
     def render(self):
         state = self.model.get_state()
@@ -33,8 +54,10 @@ class Grid(UnlockView):
             return
             
         if state.change == GridStateChange.XChange:
-            self.marker.vertices[::2] = [i + int(state.step_value)*self.icon_width for i in self.marker.vertices[::2]]
+            self.vertex_list.vertices[::2] = [i + int(state.step_value)*self.icon_width for i in self.vertex_list.vertices[::2]]
         elif state.change == GridStateChange.YChange:
-            self.marker.vertices[1::2] = [i + int(state.step_value)*self.icon_length for i in self.marker.vertices[1::2]]
+            self.vertex_list.vertices[1::2] = [i + int(state.step_value)*self.icon_height for i in self.vertex_list.vertices[1::2]]
             
+        for icon_widget in self.icon_widgets:
+            icon_widget.render()
             
