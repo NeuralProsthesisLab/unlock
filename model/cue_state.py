015286e421508789ff9c54a9a27249aa681c405a
James Percent <james@syndeticlogic.org>
2013-07-31 10:33:39 -0400

    Moved dependency creation outside of factory method and added an addition factory method for creating a RandomCueStateMachine without an indicate state.
    And added TimedStimulusCueState.

--- model/cue_state.py
+++ model/cue_state.py
@@ -1,6 +1,8 @@
+
 from unlock.util import TrialTimeState, Trigger
 from model import UnlockModel
 from random import Random
+    
 
 class RandomCueStateMachine(UnlockModel):
     def __init__(self, rand, trials, cue_states, rest_state, indicate_state):
@@ -10,6 +12,7 @@ class RandomCueStateMachine(UnlockModel):
         self.indicate_state = indicate_state
         self.rand = rand
         self.trials = trials
+        self.count = 0
         self.state = rest_state
         
     def __transition__(self, next_state):
@@ -19,14 +22,20 @@ class RandomCueStateMachine(UnlockModel):
         self.state = next_state
         return self.state.trigger
         
-    def indicate_transition(self):
+    def indicate_rest(self):
         return self.__transition__(self.rest_state)
-        
-    def rest_transition(self):
+            
+    def rest_cue(self):
+        self.count += 1
+        if self.count == self.trials:
+            return Trigger.Complete            
         return self.__transition__(self.cue_states[self.rand.randint(0, len(self.cue_states)-1)])
-        
-    def cue_transition(self):
+            
+    def cue_indicate(self):
         return self.__transition__(self.indicate_state)
+        
+    def cue_rest(self):
+        return self.__transition__(self.rest_state)
         
     def process_command(self, command):
         return self.state.process_command(command)
@@ -34,22 +43,23 @@ class RandomCueStateMachine(UnlockModel):
     def start(self):
         self.state.start()
         
-    @staticmethod
-    def create(trials, cues, cue_duration, rest_duration, indicate_duration, seed=42):
-        cue_states = []
-        for cue in cues:
-            cue_states.append(CueState.create(cue, cue_duration))
-            
-        rest_state = CueState.create(Trigger.Rest, rest_duration)
-        indicate_state = CueState.create(Trigger.Indicate, indicate_duration)
+    @staticmethod 
+    def create_cue_indicate_rest(cue_states, rest_state, indicate_state, seed=42, trials=25):
         state_machine = RandomCueStateMachine(Random(seed), trials, cue_states, rest_state, indicate_state)
-        
         for cue_state in cue_states:
-            cue_state.transition_fn = state_machine.cue_transition
-            
-        rest_state.transition_fn = state_machine.rest_transition
-        indicate_state.transition_fn = state_machine.indicate_transition
-        rest_state.start()
+            cue_state.transition_fn = state_machine.cue_indicate
+        rest_state.transition_fn = state_machine.rest_cue
+        indicate_state.transition_fn = state_machine.indicate_rest
+ #       rest_state.start()
+        return state_machine
+        
+    @staticmethod
+    def create_cue_rest(cue_states, rest_state, seed=42, trials=25):
+        state_machine = RandomCueStateMachine(Random(seed), trials, cue_states, rest_state, None)
+        for cue_state in cue_states:
+            cue_state.transition_fn = state_machine.cue_rest
+        rest_state.transition_fn = state_machine.rest_cue
+#        rest_state.start()
         return state_machine
         
         
@@ -86,4 +96,31 @@ class CueState(UnlockModel):
         time_state = TrialTimeState(duration, 0)
         return CueState(state_id, time_state)
         
-       
+        
+class TimedStimulusCueState(UnlockModel):
+    def __init__(self, timed_stimulus):
+        super(TimedStimulusCueState, self).__init__()
+        self.timed_stimulus = timed_stimulus
+        self.trigger = Trigger.Stop
+        
+    def start(self):
+        assert self.transition_fn != None
+        self.timed_stimulus.start()
+        self.state = self.timed_stimulus.get_state()
+        
+    def stop(self):
+        self.timed_stimulus.stop()
+        self.state = None
+        
+    def process_command(self, command):
+        assert self.transition_fn != None
+        ret = Trigger.Null
+        trigger_value = self.timed_stimulus.process_command(command)
+        if trigger_value == Trigger.Stop:
+            ret = self.transition_fn()
+        else:
+            self.state = self.timed_stimulus.get_state()
+            
+        return ret
+        
+        
