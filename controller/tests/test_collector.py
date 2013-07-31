7331c415b4697ec1e371cc1126f123064d5acc4c
James Percent <james@syndeticlogic.org>
2013-07-31 17:21:07 -0400

    Tweaks to test_collector and command

--- controller/tests/test_collector.py
+++ controller/tests/test_collector.py
@@ -1,5 +1,5 @@
 
-from unlock import PygletWindow, Collector
+from unlock import PygletWindow, Collector, Canvas
 
 import unittest
 import pyglet
@@ -7,13 +7,19 @@ import pyglet
 
 class CollectorTests(unittest.TestCase):       
     def testMSequenceCollector(self):
-        window = PygletWindow(fullscreen=False, show_fps=True)
-        canvas = Canvas.create(window.width, window.height)
-        collector = Collector.create_msequence_collector(window, canvas)
+        window = PygletWindow(fullscreen=True, show_fps=True)
+        collector = Collector.create_single_centered_msequence_collector(window)
         collector.activate()
         window.start()        
         window.close()
-        
+
+
+    def test2(self):
+        window = PygletWindow(fullscreen=True, show_fps=True)
+        collector = Collector.create_single_centered_msequence_collector(window)
+        collector.activate()
+        window.start()        
+        window.close()        
         
 def getSuite():
     return unittest.makeSuite(CollectorTests,'test')
