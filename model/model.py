f7d665b27e5250bc63c1c4b8b7fb2254d1b97772
James Percent <james@syndeticlogic.org>
2013-07-31 10:28:22 -0400

    Added UnlockModel.is_stopped.  Also added a few classes that are not in use yet - the goal is to move the positional state from the views to the models.

--- model/model.py
+++ model/model.py
@@ -1,4 +1,6 @@
 
+from unlock.util import DelegatorMixin
+
 
 class UnlockModel(object):
     def __init__(self, state=None):
@@ -9,6 +11,9 @@ class UnlockModel(object):
         pass
         
     def stop(self):
+        pass
+        
+    def is_stopped(self):
         pass
         
     def get_state(self):
@@ -28,5 +33,42 @@ class AlternatingBinaryStateModel(UnlockModel):
             self.state = not self.state
         return ret
             
+            
+class PositionMixin(object):
+    def __init__(self, x_offset, y_offset, anchor_x='center', anchor_y='center'):
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+        self.x_anchor = anchor_x
+        self.y_anchor = anchor_y
+            
+            
+class GraphicModel(DelegatorMixin):
+    def __init__(self, state_model, canvas, position):
+        super(GraphicModel, self).__init__()
+        self.add_delegate(state_model)
+        self.add_delegate(canvas)
+        self.add_delegate(position)
+        self.state_model = state_model
+        self.canvas = canvas
+        self.position = position
+            
+            
+class TextGraphic(GraphicModel):
+    def __init__(self, state_model, canvas, position, text, font='Helvetica', font_size=48,
+                 font_color=(255,255,255,255), group=None):
+        super(TextGraphic, self).__init__(state_model, canvas, position)
+        self.text = text
+        self.font = font
+        self.font_size = font_size
+        self.font_color = font_color
+        if len(font_color) == 3:
+            self.font_color = font_color + (255,)
+        self.group = group
+            
            
-    
+class ImageGraphic(GraphicModel):
+    def __init__(self, state_model, canvas, position, filename):
+        super(TextGraphic, self).__init__(state_model, canvas, position)
+        self.filename = filename
+        
+        
