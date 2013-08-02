ea3dd97f08d02b23a9eba4e9bcce6efaf780734a
James Percent <james@syndeticlogic.net>
2013-08-02 13:07:09 -0400

    Added a new collector experiment

--- controller/tests/test_collector.py
+++ controller/tests/test_collector.py
@@ -8,18 +8,18 @@ import pyglet
 class CollectorTests(unittest.TestCase):       
     def testMSequenceCollector(self):
         window = PygletWindow(fullscreen=True, show_fps=True)
-        collector = Collector.create_single_centered_msequence_collector(window)
+        collector = Collector.create_multi_centered_msequence_collector(window)
         collector.activate()
         window.start()        
         window.close()
 
 
-    def test2(self):
-        window = PygletWindow(fullscreen=True, show_fps=True)
-        collector = Collector.create_single_centered_msequence_collector(window)
-        collector.activate()
-        window.start()        
-        window.close()        
+#    def test2(self):
+#        window = PygletWindow(fullscreen=True, show_fps=True)
+ #       collector = Collector.create_single_centered_msequence_collector(window)
+ #       collector.activate()
+ #       window.start()        
+ #       window.close()        
         
 def getSuite():
     return unittest.makeSuite(CollectorTests,'test')
