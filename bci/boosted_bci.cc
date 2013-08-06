fcf68ba49fc068a1863f70b183253a117396e8d6
James Percent <james@syndeticlogic.org>
2013-08-06 13:22:11 -0400

    Added factory methods Python C++ wrapper.  Methods are used for creating BCI devices

--- bci/boosted_bci.cc
+++ bci/boosted_bci.cc
@@ -1,14 +1,37 @@
 #include <boost/python.hpp>
 
-#include "NonblockingBCI.hpp"
-#include "SampleBuffer.hpp"
+#include "nonblocking_bci.hpp"
+#include "fake_bci.hpp"
 
-char const* greet() {
-    return "Hello, world";
+using namespace boost::python;
+/*
+class NonblockingBCIWrapper : NonblockingBCI, wrapper<NonblockingBCI> {
+};
+*/
+
+BCI* create_fake_bci() {
+    return new FakeBCI();
+}
+
+BCI* create_enobio_bci() {
+    return new FakeBCI();
 }
 
 BOOST_PYTHON_MODULE(boosted_bci)
 {
-    using namespace boost::python;
-    def("greet", greet);
+    def("create_fake_bci", create_fake_bci, return_value_policy<manage_new_object>());
+    def("create_enobio_bci", create_enobio_bci, return_value_policy<manage_new_object>());
+    
+    
+    class_<NonblockingBCI, bases<BCI> >("NonblockingBCI", init<BCI*>())
+        .def("open", &NonblockingBCI::open)
+        .def("init", &NonblockingBCI::init)
+        .def("start", &NonblockingBCI::start)    
+        .def("acquire", &NonblockingBCI::acquire)
+        .def("getdata", &NonblockingBCI::getdata)    
+        .def("timestamp", &NonblockingBCI::timestamp)
+        .def("stop", &NonblockingBCI::stop)    
+        .def("close", &NonblockingBCI::close)             
+        ;
 }
+
