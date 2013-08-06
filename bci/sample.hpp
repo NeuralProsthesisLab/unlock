6957853c7607664311611dcff0a1a827e6089a05
James Percent <james@syndeticlogic.org>
2013-08-06 03:31:42 -0400

    Applied Emacs default c++-mode formatting

--- bci/sample.hpp
+++ bci/sample.hpp
@@ -8,83 +8,83 @@ static const size_t RING_SIZE=1048576;
 template<class T>
 class Sample {
 public:
-    Sample() : mpSample(0), mLength(0) {}
+  Sample() : mpSample(0), mLength(0) {}
     
-    Sample(const Sample &copy) : mpSample(copy.mpSample), mLength(copy.mLength) {
-    }
+  Sample(const Sample &copy) : mpSample(copy.mpSample), mLength(copy.mLength) {
+  }
     
-    ~Sample() {
-        mpSample = 0;
-        mLength = 0;
-    }
+  ~Sample() {
+    mpSample = 0;
+    mLength = 0;
+  }
     
-    Sample& operator=(const Sample& other) {
-        mpSample = other.mpSample;
-        mLength = other.mLength;
-        return *this;
-    }
+  Sample& operator=(const Sample& other) {
+    mpSample = other.mpSample;
+    mLength = other.mLength;
+    return *this;
+  }
     
-    void configure(T* pSample, int length) {
-        mpSample = pSample;
-        mLength = length;        
-    }
+  void configure(T* pSample, int length) {
+    mpSample = pSample;
+    mLength = length;        
+  }
  
-    T* sample() {
-        return mpSample;
-    }
+  T* sample() {
+    return mpSample;
+  }
     
-    size_t length() {
-        return mLength;
-    }
+  size_t length() {
+    return mLength;
+  }
     
 private:   
-    T* mpSample;
-    int mLength;
+  T* mpSample;
+  int mLength;
 };
 
 template<class T>
 class SampleBuffer {
 public:
-    SampleBuffer() : mpBuffer(0), mPosition(0) {
-        mpBuffer = new T[RING_SIZE];
-    }
+  SampleBuffer() : mpBuffer(0), mPosition(0) {
+    mpBuffer = new T[RING_SIZE];
+  }
     
-    SampleBuffer(const SampleBuffer& copy) :mpBuffer(copy.mpBuffer), mPosition(copy.mPosition) {
-    }
+  SampleBuffer(const SampleBuffer& copy) :mpBuffer(copy.mpBuffer), mPosition(copy.mPosition) {
+  }
     
-    SampleBuffer& operator=(const SampleBuffer& other) {
-        mpBuffer = other.mpBuffer;
-        mPosition = other.mPosition;
-    }
+  SampleBuffer& operator=(const SampleBuffer& other) {
+    mpBuffer = other.mpBuffer;
+    mPosition = other.mPosition;
+  }
     
-    ~SampleBuffer() {
-        BOOST_VERIFY(mpBuffer != 0);
-        delete[] mpBuffer;
-    }
+  ~SampleBuffer() {
+    BOOST_VERIFY(mpBuffer != 0);
+    delete[] mpBuffer;
+  }
 
-    size_t maximum_reservation() {
-        return RING_SIZE-1;
-    }
+  size_t maximum_reservation() {
+    return RING_SIZE-1;
+  }
     
-    T* reserve(size_t samples) {
-        BOOST_VERIFY(mpBuffer != 0 && samples < RING_SIZE);
+  T* reserve(size_t samples) {
+    BOOST_VERIFY(mpBuffer != 0 && samples < RING_SIZE);
         
-        if (samples >= RING_SIZE) {
-            return 0;
-        }
-        
-        if ((mPosition + samples) > RING_SIZE) {
-            mPosition = 0;
-        }
-        
-        T* ret = mpBuffer+mPosition;
-        mPosition += samples;
-        return ret;
+    if (samples >= RING_SIZE) {
+      return 0;
     }
+        
+    if ((mPosition + samples) > RING_SIZE) {
+      mPosition = 0;
+    }
+        
+    T* ret = mpBuffer+mPosition;
+    mPosition += samples;
+    return ret;
+  }
     
 private:
-    T* mpBuffer;
-    size_t mPosition;
+  T* mpBuffer;
+  size_t mPosition;
 };
 
 #endif
