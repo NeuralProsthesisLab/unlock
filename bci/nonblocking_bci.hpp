24c06b5b78c1969e8ca4a1b46874fc9162b96554
James Percent <james@syndeticlogic.org>
2013-08-06 04:00:36 -0400

    Applied Emacs' default c++-mode formatting

--- bci/nonblocking_bci.hpp
+++ bci/nonblocking_bci.hpp
@@ -12,22 +12,22 @@ static const size_t SAMPLE_BUFFER_SIZE=8192;
 
 class NonblockingBCI : public BCI {
 public:
-	NonblockingBCI(BCI*);
-	virtual ~NonblockingBCI();
-	virtual bool open(uint8_t[]);
-	virtual bool init(size_t);
-	virtual bool start();
-	virtual size_t acquire();
-	virtual void getdata(uint32_t* data, size_t n);
-	virtual uint64_t timestamp();
-	virtual bool stop();
-	virtual bool close();
+  NonblockingBCI(BCI*);
+  virtual ~NonblockingBCI();
+  virtual bool open(uint8_t[]);
+  virtual bool init(size_t);
+  virtual bool start();
+  virtual size_t acquire();
+  virtual void getdata(uint32_t* data, size_t n);
+  virtual uint64_t timestamp();
+  virtual bool stop();
+  virtual bool close();
 private:
-    BCI* mpBCI;
-    Sample<uint32_t>* mpSamples;
-    boost::lockfree::spsc_queue<Sample<uint32_t>*, boost::lockfree::capacity<(SAMPLE_BUFFER_SIZE-1)> > mQueue;
-    boost::thread mAsyncSampleCollector;
-    boost::atomic<bool> mDone;
+  BCI* mpBCI;
+  Sample<uint32_t> mSamples[SAMPLE_BUFFER_SIZE];
+  boost::lockfree::spsc_queue<Sample<uint32_t>*, boost::lockfree::capacity<(SAMPLE_BUFFER_SIZE-1)> > mQueue;
+  boost::thread mAsyncSampleCollector;
+  boost::atomic<bool> mDone;
 
 };
 
