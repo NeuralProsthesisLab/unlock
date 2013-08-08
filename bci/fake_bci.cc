f562dd330e346b43b271b5455dd624112ca26702
James Percent <james@syndeticlogic.org>
2013-08-08 01:06:17 -0400

    Added some innards to FakeBCI

--- bci/fake_bci.cc
+++ bci/fake_bci.cc
@@ -1,41 +1,73 @@
 #include "fake_bci.hpp"
+#include <limits>
+#include <boost/random/uniform_int_distribution.hpp>
 
-FakeBCI::FakeBCI() {
+
+
+FakeBCI::FakeBCI()
+	: mOpenCount(0), mOpenRet(true), mInitCount(0), mLastChannels(0), mInitRet(true), mStartCount(0), mStartRet(true),
+	  mAcquireCount(0), mAcquireRet(1), mGetDataCount(0), mpLastGetData(0), mLastSamples(0),
+	  mTimestampCount(0), mTimestampRet(-1), mStopCount(0), mStopRet(true), mCloseCount(0),
+	  mCloseRet(true)
+{
+	mLastMac[0] = 0xd;
+	mLastMac[1] = 0xe;
+	mLastMac[2] = 0xa;
+	mLastMac[3] = 0xd;
+	mLastMac[4] = 0xe;
+	mLastMac[5] = 0xd;
 }
 
 FakeBCI::~FakeBCI() {
-	
 }
 
-bool FakeBCI::open(uint8_t[]) {
-	return true;
+bool FakeBCI::open(uint8_t mac[]) {
+	BOOST_VERIFY(sizeof(mac) == MAC_ADDRESS_SIZE);
+	mOpenCount++;
+	std::copy(mac, mac+MAC_ADDRESS_SIZE, mLastMac);
+	return mOpenRet;
 }
 
-bool FakeBCI::init(size_t) {
-	return true;
+bool FakeBCI::init(size_t channels) {
+	mInitCount++;
+	mLastChannels = channels;
+	return mInitRet;
 }
 
 bool FakeBCI::start() {
-	return true;
+	mStartCount++;
+	return mStartRet;
 }
 
 size_t FakeBCI::acquire() {
-	return 1;
+	mAcquireCount++;
+	return mAcquireRet;
 }
 
-void FakeBCI::getdata(uint32_t* data, size_t n) {
-
+void FakeBCI::getdata(uint32_t* buffer, size_t samples) {
+	BOOST_VERIFY(buffer != 0);
+	BOOST_VERIFY(mAcquireRet == samples);
+	mGetDataCount++;
+	for(int i=0; i < samples; i++) {
+		boost::random::uniform_int_distribution<> dist(0, std::numeric_limits<uint32_t>::max());
+		buffer[i] = (uint32_t)dist(gen);
+	}
+	mpLastGetData = buffer;
+	mLastSamples = samples;
 }
 
 uint64_t FakeBCI::timestamp() {
-	return 0;
+	mTimestampCount++;
+	return mTimestampRet;
 }
 
 bool FakeBCI::stop() {
-	return true;
+	mStopCount++;
+	return mStopRet;
 }
 
 bool FakeBCI::close() {
-	return true;
+	mCloseCount++;
+	return mCloseRet;
 }
 
