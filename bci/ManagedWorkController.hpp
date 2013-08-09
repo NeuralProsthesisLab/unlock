f47c0d9b138563e8e19bf02fc4e0dd044762cdb4
James Percent <james@syndeticlogic.org>
2013-08-08 23:34:33 -0400

    Whoops - forgot to add the ManagedWorkController

---
+++ bci/ManagedWorkController.hpp
@@ -0,0 +1,18 @@
+#ifndef MANAGED_WORK_CONTROLLER_HPP
+#define MANAGED_WORK_CONTROLLER_HPP
+
+#include <boost/atomic.hpp>
+#include "IWorkController.hpp"
+
+class ManagedWorkController : public IWorkController {
+public:
+    ManagedWorkController(bool startState);
+    virtual ~ManagedWorkController();
+    virtual bool doWork();
+    void setDoWorkState(bool state);
+    
+private:
+    boost::atomic<bool> mState;
+};
+
+#endif
