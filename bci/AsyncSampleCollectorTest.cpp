9e6980f212e0022ebfeb041b130e59b1ee697c42
James Percent <james@syndeticlogic.org>
2013-08-08 23:48:58 -0400

    Added tests for AsyncSampleCollector

--- bci/AsyncSampleCollectorTest.cpp
+++ bci/AsyncSampleCollectorTest.cpp
@@ -14,7 +14,7 @@ using namespace boost::lockfree;
 
 BOOST_AUTO_TEST_SUITE(AsyncSampleCollectorTest)
 
-BOOST_AUTO_TEST_CASE(test)
+BOOST_AUTO_TEST_CASE(test_create_destroy)
 {
   FakeBci fbci;
   Sample<uint32_t>* pProducerSamples = new Sample<uint32_t>[42];
@@ -22,9 +22,29 @@ BOOST_AUTO_TEST_CASE(test)
   spsc_queue<Sample<uint32_t>*,  capacity<(42 - 1)> > queue;
   IntegralWorkController workController(1);
 
-  AsyncSampleCollector collector = AsyncSampleCollector((IBci*)&fbci, (spsc_queue<Sample<uint32_t>* >* )&queue,
+  AsyncSampleCollector* collector = new AsyncSampleCollector((IBci*)&fbci, (spsc_queue<Sample<uint32_t>* >* )&queue,
                                             &workController, pProducerSamples, &sampleRingBuffer);
-  collector();
+  delete collector;
+}
+
+BOOST_AUTO_TEST_CASE(test_create_copy)
+{
+  FakeBci fbci;
+  Sample<uint32_t>* pProducerSamples = new Sample<uint32_t>[42];
+  SampleBuffer<uint32_t> sampleRingBuffer;
+  spsc_queue<Sample<uint32_t>*,  capacity<(42 - 1)> > queue;
+  IntegralWorkController workController(1);
+  IntegralWorkController workController1(1);
+
+  AsyncSampleCollector c = AsyncSampleCollector((IBci*)&fbci, (spsc_queue<Sample<uint32_t>* >* )&queue,
+                                            &workController, pProducerSamples, &sampleRingBuffer);  
+  AsyncSampleCollector c1 = AsyncSampleCollector((IBci*)&fbci, (spsc_queue<Sample<uint32_t>* >* )&queue,
+                                            &workController1, pProducerSamples, &sampleRingBuffer);  
+  AsyncSampleCollector c2(c);
+  BOOST_VERIFY(c == c2);
+  BOOST_VERIFY(c != c1);
+  BOOST_VERIFY(!(c != c2));
+  BOOST_VERIFY(!(c == c1));  
 }
 
 BOOST_AUTO_TEST_SUITE_END()
