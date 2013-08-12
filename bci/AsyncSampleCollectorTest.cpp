ed8a4d8310dd1d48c5c1581a22856d0e5584db6e
James Percent <james@syndeticlogic.org>
2013-08-12 18:11:05 -0400

    Added a test

--- bci/AsyncSampleCollectorTest.cpp
+++ bci/AsyncSampleCollectorTest.cpp
@@ -75,4 +75,33 @@ BOOST_AUTO_TEST_CASE(test_functor)
  BOOST_CHECK_EQUAL(1, c.currentSample());
 }
 
+
+
+BOOST_AUTO_TEST_CASE(test_threaded_functor)
+{
+  cout << "AsyncSampleCollector.test_functor " << endl;
+  FakeBci fbci;
+  Sample<uint32_t>* pProducerSamples = new Sample<uint32_t>[4200];
+  Sample<uint32_t>* pConsumerSamples = new Sample<uint32_t>[4200];  
+  SampleBuffer<uint32_t> sampleRingBuffer;
+  boost::lockfree::spsc_queue<Sample<uint32_t>* > queue(4200);
+  IntegralWorkController workController(1);
+  
+  boost::thread t(AsyncSampleCollector((IBci*)&fbci, (spsc_queue<Sample<uint32_t>* >* )&queue,
+                                           &workController, pProducerSamples, &sampleRingBuffer));
+ //c();
+ t.join();
+ 
+ BOOST_CHECK_EQUAL(1, fbci.mAcquireCount);
+ BOOST_CHECK_EQUAL(1, fbci.mChannelsCount);
+ BOOST_CHECK_EQUAL(1, fbci.mGetDataCount);
+ BOOST_CHECK_EQUAL(pProducerSamples[0].sample(), fbci.mpLastGetData);
+ BOOST_CHECK_EQUAL(pProducerSamples[0].length(), fbci.mLastChannels * fbci.mAcquireRet);
+ 
+ size_t count =  queue.pop(&pConsumerSamples, 4200);
+ BOOST_CHECK_EQUAL(1, count);
+ BOOST_CHECK_EQUAL(pProducerSamples[0].sample(), pConsumerSamples[0].sample());
+ BOOST_CHECK_EQUAL(pProducerSamples[0].length(), pConsumerSamples[0].length());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
