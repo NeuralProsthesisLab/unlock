60c6dc716b975b9a71ef31e8ededc2e719ef3dc0
James Percent <james@syndeticlogic.net>
2013-08-15 15:02:24 -0400

    Added a convenience hack for testing to bci/__init__.py; copies the .dll to .pyd so Python will look for it

--- bci/__init__.py
+++ bci/__init__.py
@@ -24,15 +24,21 @@ from fakebci import *
 
 def create_so():
     base_dir = os.path.dirname(inspect.getabsfile(FakeBCI))
-    boosted_bci = os.path.join(base_dir, 'boosted_bci.so')    
-    if not os.path.exists(boosted_bci):
-        if sys.platform == 'darwin':
+
+    if sys.platform == 'darwin':
+        boosted_bci = os.path.join(base_dir, 'boosted_bci.so')
+        if not os.path.exists(boosted_bci):
             if platform.architecture()[0] == '64bit':
                 shutil.copyfile(os.path.join(base_dir, 'libboosted_bci_darwin_x86_64.so'), boosted_bci)
             else:
                 raise NotImplementedError("32 bit OS X is currently untested")
-                
-                
+            
+    if sys.platform == 'win32':
+        boosted_bci = os.path.join(base_dir, 'boosted_bci.pyd')
+        if not os.path.exists(boosted_bci):
+            shutil.copyfile(os.path.join(base_dir, 'boosted_bci_win_x86.dll'), boosted_bci)            
+        os.environ['PATH']=os.environ['PATH']+';'+base_dir+'\\boost\\win-x86\\lib'
+            
 try:
     import boosted_bci
 except:
