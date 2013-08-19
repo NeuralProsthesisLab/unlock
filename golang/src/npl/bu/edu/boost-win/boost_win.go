c41e9a603b42452b6320dc4badda771fefa0cd0b
James Percent <james@syndeticlogic.net>
2013-08-19 19:21:39 -0400

    Working boost_win.go project.
    Also deleted some old binaries

--- golang/src/npl/bu/edu/boost-win/boost_win.go
+++ golang/src/npl/bu/edu/boost-win/boost_win.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-   // "npl/bu/edu/unzip"
+    "npl/bu/edu/unzip"
     "os"
     "os/exec"
     "log"
@@ -17,21 +17,19 @@ func cwdAbs() string {
 }
 
 func main() {
-  //  current_working_directory := cwdAbs()
-//    log_file := current_working_directory + "\\" + 
     logf, err := os.OpenFile("boost-win.log", os.O_WRONLY|os.O_CREATE,0640)
     if err != nil {
         log.Fatalln("Failed to create boost-win.log", err)
     }
     log.SetOutput(io.MultiWriter(logf, os.Stdout))
     
-    log.Println(" Version 7 ")
-    /*
+    log.Println(" Version 0.91 ")
+
     u := &unzip.Unzip{"boost_1_54_0.zip", "", nil}
     if err := u.Expand(); err != nil {
         log.Fatalln("Failed to expand boost_1_54_0.zip ", err)
     }
-*/
+
     boost_dir := cwdAbs()
     
     if err := os.Chdir("boost_1_54_0"); err != nil {
@@ -42,19 +40,16 @@ func main() {
    bootstrap_cmd := "/C "+build_dir+"\\"+"bootstrap"
    log.Println("Executing: "+bootstrap_cmd)
     cmd := exec.Command("cmd", bootstrap_cmd)
-  /*  if err := cmd.Run(); err != nil {
+    if err := cmd.Run(); err != nil {
         log.Fatalln("Failed to execute boostrap command: "+bootstrap_cmd, err)
     }
     log.Println("Success: "+bootstrap_cmd)
-    */
-    
-//    b2_cmd :=  "/C "+build_dir+"\\"+"b2 --prefix="+boost_dir+"\\win-x86-64 --exec-prefix="+boost_dir+"\\win-x86-64  --includedir="+boost_dir+"\\win-x86-64 --libdir="+boost_dir+"\\win-x86-64 --with-thread --with-test --with-python"
-//    b2_cmd :=  "/C "+build_dir+"\\"+"b2  --prefix="+boost_dir+"\\win-x86-64 --stagedir="+boost_dir+"\\win-x86-64 --with-thread --with-test --with-python"
-    b2_cmd :=  "/C "+build_dir+"\\"+"b2 install --prefix="+boost_dir+"\\win-x86-64 --with-thread --with-test --with-python "
+
+    b2_cmd :=  "/C "+build_dir+"\\"+"b2 install --prefix="+boost_dir+"\\win-x86  address-model=32  runtime-link=shared link=shared"
     log.Println("Executing: "+b2_cmd)    
     cmd = exec.Command("cmd", b2_cmd)
     if err := cmd.Run(); err != nil {
         log.Fatalln("Failed to execute b2: ", err)
     }
-    log.Println("Success: "+b2_cmd)    
+
 }
