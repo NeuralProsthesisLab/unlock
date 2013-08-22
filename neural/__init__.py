5c2bd709ad90b55803f9bff34646244ef7143208
James Percent <james@syndeticlogic.org>
2013-08-22 17:56:08 -0400

    Added neural/__init__.py

--- neural/__init__.py
+++ neural/__init__.py
@@ -4,6 +4,9 @@ import platform
 import shutil
 import inspect
 
+from acuqire import *
+from classify import *
+
 #
 #def machine():
 #    """Return type of machine."""
@@ -19,27 +22,27 @@ import inspect
 #    return machine2bits.get(machine, None)
 #
 #print (os_bits())
-
-from fakebci import *
-
-def create_so():
-    base_dir = os.path.dirname(inspect.getabsfile(FakeBCI))
-
-    if sys.platform == 'darwin':
-        boosted_bci = os.path.join(base_dir, 'boosted_bci.so')
-        if not os.path.exists(boosted_bci):
-            if platform.architecture()[0] == '64bit':
-                shutil.copyfile(os.path.join(base_dir, 'libboosted_bci_darwin_x86_64.so'), boosted_bci)
-            else:
-                raise NotImplementedError("32 bit OS X is currently untested")
-            
-    if sys.platform == 'win32':
-        boosted_bci = os.path.join(base_dir, 'boosted_bci.pyd')
-        if not os.path.exists(boosted_bci):
-            shutil.copyfile(os.path.join(base_dir, 'boosted_bci_win_x86.dll'), boosted_bci)            
-        os.environ['PATH']=os.environ['PATH']+';'+base_dir+'\\boost\\win-x86\\lib'
-            
-try:
-    import boosted_bci
-except:
-    print "Platform specific bci files have not been created"
+#
+#from fakebci import *
+#
+#def create_so():
+#    base_dir = os.path.dirname(inspect.getabsfile(FakeBCI))
+#
+#    if sys.platform == 'darwin':
+#        boosted_bci = os.path.join(base_dir, 'boosted_bci.so')
+#        if not os.path.exists(boosted_bci):
+#            if platform.architecture()[0] == '64bit':
+#                shutil.copyfile(os.path.join(base_dir, 'libboosted_bci_darwin_x86_64.so'), boosted_bci)
+#            else:
+#                raise NotImplementedError("32 bit OS X is currently untested")
+#            
+#    if sys.platform == 'win32':
+#        boosted_bci = os.path.join(base_dir, 'boosted_bci.pyd')
+#        if not os.path.exists(boosted_bci):
+#            shutil.copyfile(os.path.join(base_dir, 'boosted_bci_win_x86.dll'), boosted_bci)            
+#        os.environ['PATH']=os.environ['PATH']+';'+base_dir+'\\boost\\win-x86\\lib'
+#            
+#try:
+#    import boosted_bci
+#except:
+#    print "Platform specific bci files have not been created"
