d270a68eea64dcc3172ad60b92cd1d1df0b52120
James Percent <james@syndeticlogic.net>
2013-08-22 02:55:36 -0400

    Modified install-win.  Represents a configurable, working installation, on Windows, of all the dependencies of the unlock framework, minus the framework package itself, which has not been updated.

--- golang/src/npl/bu/edu/install-win/install.go
+++ golang/src/npl/bu/edu/install-win/install.go
@@ -5,6 +5,7 @@ import (
     "net/http"
     "fmt"
     "io/ioutil"
+    "flag"
     "os"
     "os/exec"
     "log"
@@ -14,8 +15,7 @@ import (
 
 func runCommand(command string, errorMsg string, failOnError bool) bool {
     result := true
-    cwd := getWorkingDirectoryAbsolutePath()
-    cmd := exec.Command(`cmd`, `/C `+cwd+"\\"+command) // note the mixing of ` and "; my editor `\` are not friends
+    cmd := exec.Command("cmd", "/C", command) // note the mixing of ` and "; my editor `\` are not friends
     if err := cmd.Run(); err != nil {
         if failOnError == true {
             log.Fatalln(`FATAL: `+errorMsg+`; command = `+command, err)
@@ -29,6 +29,7 @@ func runCommand(command string, errorMsg string, failOnError bool) bool {
 
 func install(command string, packageName string, failOnError bool) {
     log.Print(`Installing `+packageName+`: `)
+    log.Println("command = "+ command)
     result := runCommand(command, `Failed to install `+packageName, failOnError)
     if result {
         fmt.Println(`Success`)
@@ -50,8 +51,8 @@ func unzipExpand(fileName string) {
     }    
 }
 
-
 func downloadAndWriteFile(url string, fileName string) {
+    log.Println("Downloading file "+fileName)
     resp, err := http.Get(url)
     if err != nil {
         log.Fatalln(err)
@@ -82,21 +83,26 @@ func installZippedPythonPackage(pythonPath string, baseUrl string, fileName stri
     downloadAndWriteFile(baseUrl+fileName, fileName)
     unzipExpand(fileName)
     chdirFailOnError(packageDirectory, `Failed to install `+packageName)
-    command := `/C `+pythonPath+` setup.py install`
+    log.Println("CWD = "+getWorkingDirectoryAbsolutePath())
+    command := pythonPath+` setup.py install`
     install(command, packageName, true)
     os.Chdir(`..`)
 }
 
-func installPython(baseUrl string, pythonInstallerName string, pythonBasePath string, pythonPackageName string) {// base_python_path string) {
-    command := `msiexec /i ` + pythonInstallerName + ` TARGETDIR=`+pythonBasePath+` /qb ALLUSERS=0 `
+func installPython(baseUrl string, pythonInstallerName string, pythonBasePath string, pythonPackageName string) {
+    cwd := getWorkingDirectoryAbsolutePath()
     log.Println(`Downloading `+pythonPackageName+`...`)
-    downloadAndWriteFile(baseUrl+pythonInstallerName, pythonInstallerName)        
-    install(command, pythonPackageName, true)
+    downloadAndWriteFile(baseUrl+pythonInstallerName, pythonInstallerName)
+    log.Println(`Installing `+pythonPackageName)
+    cmd := exec.Command("cmd", "/C", "msiexec /i ", cwd+"\\"+pythonInstallerName,`TARGETDIR=`+pythonBasePath,`/qb`, `ALLUSERS=0`)
+    if err := cmd.Run(); err != nil {
+        log.Fatalln(`FATAL: failed to install python `, err)
+    }
 }
 
 func installEasyInstall(baseUrl string, pythonPath string) {
-    downloadAndWriteFile(baseUrl+`distribute_setup.py`, `distribute.py`)
-    install(pythonPath+` distribute_setup.py`, `easy_install`, true)    
+    downloadAndWriteFile(baseUrl+`distribute_setup-py`, `distribute_setup.py`)
+    install(pythonPath+` distribute_setup.py`, `easy_install`, true)
 }
 
 func installPip(easyInstallPath string) {
@@ -109,17 +115,21 @@ func installVirtualenv(pipPath string) {
 
 func createVirtualenv(unlockDirectory string, virtualenvPath string, envName string) {
     errorMsg := `Failed to create virtual environment`
+    var cwd = getWorkingDirectoryAbsolutePath()
     chdirFailOnError(unlockDirectory, errorMsg)
-    command := virtualenvPath+` --distribute `+envName //python27`
+    command := virtualenvPath+` --system-site-packages `+envName //python27`
     runCommand(command, errorMsg, true)
+    os.Chdir(cwd)
 }
 
 func installPyglet12alpha(pythonPath string, baseUrl string, fileName string, packageName string, packageDirectory string) {
     installZippedPythonPackage(pythonPath, baseUrl, fileName, packageName, packageDirectory)
 }
 
-func installNumPy(pipPath string) {
-    install(pipPath+` numpy`, `numpy`, true)
+func installNumPy(baseUrl string, numpyPath string) {
+    downloadAndWriteFile(baseUrl+numpyPath, numpyPath)//numpy-MKL-1.7.1.win32-py2.7.exe)
+    var cwd = getWorkingDirectoryAbsolutePath()
+    install(cwd+"\\"+numpyPath, `numpy`, true)
 }
 
 func installPySerial26(pythonPath string, baseUrl string, fileName string, packageName string, packageDirectory string) {
@@ -152,23 +162,50 @@ func installUnlock() {
     */
 }
 
- 
+//var species = flag.String("species", "gopher", "the species we are studying")
+
+// Example 2: Two flags sharing a variable, so we can have a shorthand.
+// The order of initialization is undefined, so make sure both use the
+// same default value. They must be set up with an init function.
+var confFile = flag.String("conf", "", "Qualified file name of Unlock installation configuration file")
+
+func createConf() UnlockInstallConf {
+    if *confFile == `` {
+        return UnlockInstallConf {`C:\Unlock`, `http://jpercent.org/`, `python-2.7.5.msi`,
+            `Python-2.7.5`, `C:\Python27`, `C:\Python27\python.exe`, `numpy-MKL-1.7.1.win32-py2.7.exe`,
+            `C:\Python27\Scripts\easy_install.exe`, `C:\Python27\Scripts\pip.exe`,
+            `C:\Python27\Scripts\virtualenv.exe`, `python27`, 
+            `C:\Unlock\python27\Scripts\python.exe`, `C:\Unlock\python27\Scripts\pip.exe`,
+            `pyglet-1.2alpha.zip`, `pyglet-1.2alpha`, `pyglet-1.2alpha1`,
+            `pyserial-2.6.zip`, `pyserial-2.6`, `pyserial-2.6`}
+    } else {
+        return ParseConf(`config.json`)
+    }    
+}
+
 func main() {
+
+    flag.Parse()
     logf, err := os.OpenFile(`unlock-install.log`, os.O_WRONLY|os.O_CREATE,0640)
     if err != nil {
         log.Fatalln(err)
     }
     
     log.SetOutput(io.MultiWriter(logf, os.Stdout))
-    conf := ParseConf("config.json")
+    log.Printf("conf file = "+*confFile)
+    
+    var conf = createConf()
 
     installPython(conf.BaseUrl, conf.PythonInstallerName, conf.PythonBasePath, conf.PythonPackageName)
+    installNumPy(conf.BaseUrl, conf.NumpyPackageName)
     installEasyInstall(conf.BaseUrl, conf.PythonPath)
     installPip(conf.EasyInstallPath)
     installVirtualenv(conf.PipPath)
+    if err := os.MkdirAll(conf.UnlockDirectory, 0755); err != nil {
+        log.Fatalln(`Failed to create `+conf.UnlockDirectory, err)
+    }
     createVirtualenv(conf.UnlockDirectory, conf.VirtualenvPath, conf.EnvName)
-    installPyglet12alpha(conf.PythonPath, conf.BaseUrl, conf.PygletZipName, conf.PygletPackageName, conf.PygletDirectory)
-    installNumPy(conf.PipPath)
-    installPySerial26(conf.PythonPath, conf.BaseUrl, conf.PyserialZipName, conf.PyserialPackageName, conf.PyserialDirectory)
+    installPyglet12alpha(conf.EnvPythonPath, conf.BaseUrl, conf.PygletZipName, conf.PygletPackageName, conf.PygletDirectory)
+    installPySerial26(conf.EnvPythonPath, conf.BaseUrl, conf.PyserialZipName, conf.PyserialPackageName, conf.PyserialDirectory)
     installUnlock()
 }
