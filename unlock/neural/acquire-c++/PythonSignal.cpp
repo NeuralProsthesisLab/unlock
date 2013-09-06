a71703d453a2c881aa9a952616830b5ffb026ef0
James Percent <james@syndeticlogic.net>
2013-09-06 01:05:54 -0400

    Whoops - forgot to add these.

---
+++ unlock/neural/acquire-c++/PythonSignal.cpp
@@ -0,0 +1,64 @@
+#include <boost/random/uniform_int_distribution.hpp>
+#include <limits>
+#include <iostream>
+
+#include "PythonSignal.hpp"
+
+PythonSignal::PythonSignal(ISignal* pSignal)
+  : mpSignal(pSignal)
+{
+}
+
+PythonSignal::~PythonSignal() {
+  delete mpSignal;
+}
+
+bool PythonSignal::open(/* boost::python::list macAddress */) {
+  uint8_t mac[6] = { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6 };
+  return mpSignal->open(mac);
+}
+
+bool PythonSignal::init(size_t channels) {
+  return mpSignal->init(channels);
+}
+
+size_t PythonSignal::channels() {
+    return mpSignal->channels();  
+}
+
+bool PythonSignal::start() {
+    return mpSignal->start();  
+}
+
+size_t PythonSignal::acquire() {
+    return mpSignal->acquire();
+}
+
+std::vector<uint32_t> PythonSignal::getdata(size_t samples) {
+    std::vector<uint32_t> ret = std::vector<uint32_t>();
+    try {
+      uint32_t* buffer = new uint32_t[samples];
+      mpSignal->getdata(buffer, samples);
+      for (size_t i = 0; i < samples; i++) {
+        ret.push_back(buffer[i]);
+      }
+      delete[] buffer;
+      return ret;
+  } catch(...) {
+    std::cerr << "PythonSignal.getdata: ERROR: exception raised; returning empty samples vector " << std::endl;
+  }
+  return ret;
+}
+
+uint64_t PythonSignal::timestamp() {
+  return mpSignal->timestamp();
+}
+
+bool PythonSignal::stop() {
+  return mpSignal->stop();
+}
+
+bool PythonSignal::close() {
+  return mpSignal->close();
+}
+
---
+++ unlock/neural/acquire-c++/PythonSignalTest.cpp
@@ -0,0 +1,35 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/thread/thread.hpp>
+#include <iostream>
+
+#include "PythonSignal.hpp"
+#include "FakeSignal.hpp"
+#include "Sample.hpp"
+
+using namespace std;
+
+
+BOOST_AUTO_TEST_SUITE(PythonSignalTest)
+
+BOOST_AUTO_TEST_CASE(pythion_signal_test_create_delete)
+{
+  cout << "PythonSignalTest.test_create_delete" << endl;
+  PythonSignal* signal = new PythonSignal(new FakeSignal());
+  delete signal;//blockless_signal;
+}
+
+BOOST_AUTO_TEST_CASE(python_signal_test_create_open_start_acquire_getdata_stop_close)
+{
+  cout << "Pynobio.test_create_open_start_acquire_getdata_stop_close " << endl;
+  PythonSignal* signal = new PythonSignal(new FakeSignal());
+  cout << "Before mac addr " << endl;
+
+  BOOST_VERIFY(signal->open(/*macAddress*/));
+  BOOST_VERIFY(signal->start());
+  cout << "After start " << endl;
+  BOOST_VERIFY(signal->stop());
+  BOOST_VERIFY(signal->close());
+  cout << "After close " << endl;  
+}
+
+BOOST_AUTO_TEST_SUITE_END()
