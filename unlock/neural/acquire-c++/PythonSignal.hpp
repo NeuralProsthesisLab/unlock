14507365f4882c7606c0cb14c0efa5bc170fa2b0
James Percent <james@syndeticlogic.net>
2013-09-06 01:05:54 -0400

    Whoops - forgot to add these.

---
+++ unlock/neural/acquire-c++/PythonSignal.hpp
@@ -0,0 +1,31 @@
+#ifndef PYTHON_SIGNAL_HPP
+#define PYTHON_SIGNAL_HPP
+
+#include <boost/python.hpp>
+#include <vector>
+
+#include <stdint.h>
+#include <cstddef>
+
+#include "Portability.hpp"
+#include "ISignal.hpp"
+
+class DllExport PythonSignal
+{
+ public:
+  PythonSignal(ISignal* pSignal);
+  virtual ~PythonSignal();
+  bool open(/* boost::python::list mac */);
+  bool init(size_t channels);
+  size_t channels();
+  bool start();
+  size_t acquire();
+  std::vector<uint32_t> getdata(size_t samples);
+  uint64_t timestamp();
+  bool stop();
+  bool close();
+private:
+    ISignal* mpSignal;
+};
+
+#endif
