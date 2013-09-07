6190763f0d495f33409f19f8040a2c749d76789d
James Percent <james@syndeticlogic.net>
2013-09-07 19:17:45 -0400

    Purged code of BCI references

--- unlock/controller/test/test_dashboard.py
+++ unlock/controller/test/test_dashboard.py
@@ -3,20 +3,11 @@ from unlock.controller import PygletWindow, Collector, Dashboard, Canvas
 
 import unittest
 import pyglet
-from unlock.bci import FakeBCI
+from unlock.neural import RandomSignal
 
 class DashboardTests(unittest.TestCase):       
     def testDashboard(self):
-        window = PygletWindow(fullscreen=False, show_fps=True)
-        bci = FakeBCI()
-        collector = Collector.create_multi_centered_msequence_collector(window, bci, standalone=False)
-        collector1 = Collector.create_single_centered_msequence_collector(window, bci, standalone=False)
-
-        dashboard = Dashboard.create(window, [collector, collector1], bci)
-        dashboard.activate()
-        window.start()        
-        window.close()                        
-
+        pass
         
 def getSuite():
     return unittest.makeSuite(CollectorTests,'test')
--- unlock/controller/test/test_emg_collector.py
+++ unlock/controller/test/test_emg_collector.py
@@ -1,6 +1,6 @@
 
 from unlock import PygletWindow, Collector, Canvas
-from unlock.bci import FakeBCI
+from unlock.neural import RandomSignal
 
 import unittest
 import pyglet
@@ -8,49 +8,7 @@ import pyglet
 
 class CollectorTests(unittest.TestCase):       
     def testEMGSequenceCollector(self):
-        
-        try:
-           # from pynobio import Enobio
-            #pynobio = True
-            pynobio = False
-            fakebci = False
-        except Exception:
-            pynobio = False
-            fakebci = True
-            
-        
-        if pynobio:
-            try:
-                bci = Enobio()
-                bci.channels = 8
-                if not bci.open():
-                    print("enobio did not open")
-                    raise Exception('enobio did not open')
-                if not bci.start():
-                    print('enobio device did not start streaming')                                 
-                    raise Exception('enobio device did not start streaming')                       
-            except Exception as e:
-                raise e
-                fakebci = True
-
-        ##if fakebci:
-        #from unlock.bci import FakeBCI
-        bci = FakeBCI()
-            
-            
-        window = PygletWindow(fullscreen=True, show_fps=True)
-        collector = Collector.create_emg_collector(window, bci, radius=.75)
-        collector.activate()
-        window.start()        
-        window.close()
-
-
-#    def test2(self):
-#        window = PygletWindow(fullscreen=True, show_fps=True)
- #       collector = Collector.create_single_centered_msequence_collector(window)
- #       collector.activate()
- #       window.start()        
- #       window.close()        
+        pass
         
 def getSuite():
     return unittest.makeSuite(CollectorTests,'test')
