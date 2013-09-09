8fcd915a77580b365ea3ff70085cc7bb2cc2f85e
James Percent <james@syndeticlogic.net>
2013-09-08 22:48:28 -0400

    Added the python33 dll to the acquire directore and added code in the setup to build it into the package.  It should be installed by the Python33 installer, but in case it is not, it will be were the C++ extention can find it.

--- setup.py
+++ setup.py
@@ -12,17 +12,20 @@ packages = ['unlock',
 
 package_data = {
                 'unlock' : ['conf.json', 'README.md'],
-                'unlock.controller' : ['resource/analyzer.jpg', 'resource/Arrow.png', 'resource/ArrowSel.png', 'resource/emg-100x100.jpg', 'resource/emg.jpg', 'resource/IRCodes.txt', 'resource/LazerToggle.png', 'resource/LazerToggleS.png', 'resource/rsz_analyzer.jpg', 'resource/scope.png', 'resource/tv.png'],
+                'unlock.controller' : ['resource/analyzer.jpg', 'resource/Arrow.png', 'resource/ArrowSel.png', 'resource/emg-100x100.jpg',
+                                       'resource/emg.jpg', 'resource/IRCodes.txt', 'resource/LazerToggle.png', 'resource/LazerToggleS.png',
+                                       'resource/rsz_analyzer.jpg', 'resource/scope.png', 'resource/tv.png'],
                 'unlock.context.test' : ['app-ctx.xml'], 
                  'unlock.model' : [],
 
 # win-x86 libs find acquire-c++/boost/win-x86/lib | grep -v -e lib$  -e gd | sed s/^/\'/ | sed s/$/\',/
-                'unlock.neural' : ['acquire/boost_chrono-vc110-mt-1_54.dll', 'acquire/boost_date_time-vc110-mt-1_54.dll',
-                            'acquire/boost_python-vc110-mt-1_54.dll', 'acquire/boost_system-vc110-mt-1_54.dll',
-                            'acquire/boost_thread-vc110-mt-1_54.dll', 'acquire/boost_unit_test_framework-vc110-mt-1_54.dll',
-                            'acquire/Enobio3GAPI.dll', 'acquire/QtCore4.dll', 'acquire/WinBluetoothAPI.dll',
-                            'acquire/msvcr110.dll', 'acquire/msvcp110.dll',
-                            'acquire/neuralsignal-unit-tests.exe', 'acquire/neuralsignal.pyd',
+                'unlock.neural' : ['acquire/boost_atomic-vc100-mt-1_54.dll', 'acquire/boost_chrono-vc100-mt-1_54.dll',
+                                   'acquire/boost_date_time-vc100-mt-1_54.dll', 'acquire/boost_python3-vc100-mt-1_54.dll',
+                                   'acquire/boost_random-vc100-mt-1_54.dll', 'acquire/boost_system-vc100-mt-1_54.dll',
+                                   'acquire/boost_thread-vc100-mt-1_54.dll', 'acquire/boost_unit_test_framework-vc100-mt-1_54.dll',
+                                   'acquire/Enobio3GAPI.dll', 'acquire/QtCore4.dll', 'acquire/WinBluetoothAPI.dll',
+                                   'acquire/msvcr100.dll', 'acquire/msvcp100.dll', 'acquire/python33.dll',
+                                   'acquire/neuralsignal-unit-tests.exe', 'acquire/neuralsignal.pyd',
 
 # mac os x libs find acquire-c++/boost/macosx-x86-64/lib | grep -v -e lib$  -e gd | sed s/^/\'/ | sed s/$/\',/ 
                             #'acquire/neuralsignal.so'
