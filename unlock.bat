6fd44b177cba63522efd5fc835d2dfd026ce194c
James Percent <james@syndeticlogic.org>
2013-09-26 02:48:13 -0400

    Removed unused file

--- unlock.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-setx PYTHONPATH "C:\Unlock\python27;C:\Unlock\python27\Lib\;C:\Unlock\python27\Scripts\;C:\Python27\DLLs"
-c:\Unlock\python27\Scripts\python.exe c:\Unlock\python27\Lib\site-packages\unlock\unlock.py --help %*
-pause
