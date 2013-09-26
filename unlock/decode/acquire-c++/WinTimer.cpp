8d5e3af04326ae193b27360a79b569bf37299d0a
James Percent <james@syndeticlogic.org>
2013-09-26 02:37:06 -0400

    Added attribution to code

--- unlock/decode/acquire-c++/WinTimer.cpp
+++ unlock/decode/acquire-c++/WinTimer.cpp
@@ -1,3 +1,31 @@
+
+// Copyright (c) James Percent and Unlock contributors.
+// All rights reserved.
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//    1. Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//    
+//    2. Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//    3. Neither the name of Unlock nor the names of its contributors may be used
+//       to endorse or promote products derived from this software without
+//       specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+// ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include <iostream>
 #include "WinTimer.hpp"
 
@@ -49,40 +77,3 @@ int64_t WinTimer::getFrequency() {
 int64_t WinTimer::getStartValue() {
   return mCounterStart.QuadPart;
 }
-
-/*
-int main()
-{
-  //Increasing the accuracy of Sleep to 1ms using timeBeginPeriod
-//  timeBeginPeriod(1); //Add Winmm.lib in Project
-  unsigned int diffTime = 0, lastTime = 0, newTime = 0;
-  startTime();
-  lastTime = calculateElapsedTime();
-  cout<<"Start Time = "<<lastTime<<endl;
-
-  Sleep(100);
-  newTime = calculateElapsedTime();
-  diffTime = newTime - lastTime;
-  cout<<"Time after 100ms Sleep = "<<newTime<<", Difference = "<<diffTime<<endl;
-  lastTime = newTime;
-
-  Sleep(100);
-  newTime = calculateElapsedTime();
-  diffTime = newTime - lastTime;
-  cout<<"Time after 100ms Sleep = "<<newTime<<", Difference = "<<diffTime<<endl;
-  lastTime = newTime;
-
-  Sleep(5);
-  newTime = calculateElapsedTime();
-  diffTime = newTime - lastTime;
-  cout<<"Time after   5ms Sleep = "<<newTime<<", Difference = "<<diffTime<<endl;
-  lastTime = newTime;
-
-  Sleep(50);
-  newTime = calculateElapsedTime();
-  diffTime = newTime - lastTime;
-  cout<<"Time after  50ms Sleep = "<<newTime<<", Difference = "<<diffTime<<endl;
-
-  timeEndPeriod(1); //Must be called if timeBeginPeriod() was called
-  return 0;
-} */
