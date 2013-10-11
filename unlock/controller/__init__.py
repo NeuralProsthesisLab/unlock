db1aff99ba5cf87f5f44f6123fa00e5daf4f8662
James Percent <james@syndeticlogic.net>
2013-10-10 23:57:25 -0400

    Added Calibrate to the controller package

--- unlock/controller/__init__.py
+++ unlock/controller/__init__.py
@@ -25,6 +25,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from .controller import *
-#from .collector import *
+from .calibrate import *
 from .dashboard import *
 from .fastpad import *
