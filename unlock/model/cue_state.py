8fb930d788ae000d79b9d04868f38aa0dac2f365
James Percent <james@syndeticlogic.net>
2013-10-10 20:44:29 -0400

    Hacked cue_state to support sequential and random state transitions

--- unlock/model/cue_state.py
+++ unlock/model/cue_state.py
@@ -30,7 +30,7 @@ from .model import UnlockModel
 from random import Random
     
 
-class RandomCueStateMachine(UnlockModel):
+class CueStateMachine(UnlockModel):
     def __init__(self, rand, trials, cue_states, rest_state, indicate_state):
         super(UnlockModel, self).__init__()
         self.cue_states = cue_states
@@ -40,6 +40,7 @@ class RandomCueStateMachine(UnlockModel):
         self.trials = trials
         self.count = 0
         self.state = rest_state
+        self.next_cue_state = 0
         
     def __transition__(self, next_state):
         self.state.stop()
@@ -54,8 +55,15 @@ class RandomCueStateMachine(UnlockModel):
     def rest_cue(self):
         self.count += 1
         if self.count == self.trials:
-            return Trigger.Complete            
-        return self.__transition__(self.cue_states[self.rand.randint(0, len(self.cue_states)-1)])
+            return Trigger.Complete
+        if self.rand != None:
+            next_ = self.__transition__(self.cue_states[self.rand.randint(0, len(self.cue_states)-1)])
+        else:
+            next_ = self.__transition__(self.cue_states[next_cue_state])
+            next_state += 0
+            if self.next_cue_state == len(self.cue_states):
+                self.next_cue_state = 0
+        return next_
             
     def cue_indicate(self):
         return self.__transition__(self.indicate_state)
@@ -70,25 +78,34 @@ class RandomCueStateMachine(UnlockModel):
         self.state.start()
         
     @staticmethod 
-    def create_cue_indicate_rest(cue_states, rest_state, indicate_state, seed=42, trials=25):
-        state_machine = RandomCueStateMachine(Random(seed), trials, cue_states, rest_state, indicate_state)
+    def create_random_cue_indicate_rest(cue_states, rest_state, indicate_state, seed=42, trials=25):
+        state_machine = CueStateMachine(Random(seed), trials, cue_states, rest_state, indicate_state)
         for cue_state in cue_states:
             cue_state.transition_fn = state_machine.cue_indicate
         rest_state.transition_fn = state_machine.rest_cue
         indicate_state.transition_fn = state_machine.indicate_rest
  #       rest_state.start()
         return state_machine
+    
+    @staticmethod 
+    def create_sequential_cue_indicate_rest(cue_states, rest_state, indicate_state, trials=10):
+        state_machine = CueStateMachine(None, trials, cue_states, rest_state, indicate_state)
+        for cue_state in cue_states:
+            cue_state.transition_fn = state_machine.cue_indicate
+        rest_state.transition_fn = state_machine.rest_cue
+        indicate_state.transition_fn = state_machine.indicate_rest
+        return state_machine
         
     @staticmethod
-    def create_cue_rest(cue_states, rest_state, seed=42, trials=25):
-        state_machine = RandomCueStateMachine(Random(seed), trials, cue_states, rest_state, None)
+    def create_random_cue_rest(cue_states, rest_state, seed=42, trials=25):
+        state_machine = CueStateMachine(Random(seed), trials, cue_states, rest_state, None)
         for cue_state in cue_states:
             cue_state.transition_fn = state_machine.cue_rest
         rest_state.transition_fn = state_machine.rest_cue
 #        rest_state.start()
         return state_machine
-        
-        
+    
+  
 class CueState(UnlockModel):
     def __init__(self, trigger, trial_time_state, transition_fn=None):
         super(CueState, self).__init__()
