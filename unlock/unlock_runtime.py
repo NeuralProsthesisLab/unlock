6c64d1fee8d817833a2d1ae484c70a8302ebab88
James Percent <james@syndeticlogic.net>
2013-10-24 00:51:31 -0400

    Remove temporary decoder h4x0r

--- unlock/unlock_runtime.py
+++ unlock/unlock_runtime.py
@@ -238,8 +238,6 @@ class UnlockRuntime(object):
                 self.args['receiver'] = options.receiver
             if options.decoder != None:
                 self.args['decoder'] = options.decoder
-            else:
-                self.args['decoder'] = 'inline'
                 
             self.__configure_logging__()
             self.__create_controllers__()
