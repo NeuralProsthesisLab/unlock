d9f97d5edb6c1723d236d77c5d1966e8233f8e9d
James Percent <james@syndeticlogic.net>
2013-10-24 23:53:58 -0400

    Added gridspeak sound files to setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ package_data = {
                                        'resource/rsz_analyzer.jpg', 'resource/scope.png', 'resource/tv.png'],
                 'unlock.context.test' : ['app-ctx.xml'], 
                  'unlock.model' : [],
-
 # win-x86 libs find acquire-c++/boost/win-x86/lib | grep -v -e lib$  -e gd | sed s/^/\'/ | sed s/$/\',/
                 'unlock.decode' : ['acquire/boost_atomic-vc100-mt-1_54.dll', 'acquire/boost_chrono-vc100-mt-1_54.dll',
                                    'acquire/boost_date_time-vc100-mt-1_54.dll', 'acquire/boost_python3-vc100-mt-1_54.dll',
@@ -26,11 +25,38 @@ package_data = {
                                    'acquire/Enobio3GAPI.dll', 'acquire/QtCore4.dll', 'acquire/WinBluetoothAPI.dll',
                                    'acquire/msvcr100.dll', 'acquire/msvcp100.dll', 'acquire/python33.dll',
                                    'acquire/neuralsignal-unit-tests.exe', 'acquire/neuralsignal.pyd',
-
 # mac os x libs find acquire-c++/boost/macosx-x86-64/lib | grep -v -e lib$  -e gd | sed s/^/\'/ | sed s/$/\',/ 
                             #'acquire/neuralsignal.so'
                             ],
-                 'unlock.view' : ['bell-ring-01.mp3', 'unlock.png']}
-
+                 'unlock.view' : ['bell-ring-01.mp3', 'unlock.png',
+                                  'resource/Female/sounds/alone.wav', 'resource/Female/sounds/bored.wav',
+                                  'resource/Female/sounds/down.wav', 'resource/Female/sounds/explain.wav',
+                                  'resource/Female/sounds/get.wav', 'resource/Female/sounds/goodbye.wav',
+                                  'resource/Female/sounds/hello.wav', 'resource/Female/sounds/help.wav',
+                                  'resource/Female/sounds/howareyou.wav', 'resource/Female/sounds/hungry.wav',
+                                  'resource/Female/sounds/left.wav', 'resource/Female/sounds/move.wav',
+                                  'resource/Female/sounds/no.wav', 'resource/Female/sounds/nose.wav',
+                                  'resource/Female/sounds/pain.wav', 'resource/Female/sounds/repeat.wav',
+                                  'resource/Female/sounds/right.wav', 'resource/Female/sounds/sorry.wav',
+                                  'resource/Female/sounds/thanks.wav', 'resource/Female/sounds/thirsty.wav',
+                                  'resource/Female/sounds/up.wav', 'resource/Female/sounds/when.wav',
+                                  'resource/Female/sounds/where.wav', 'resource/Female/sounds/who.wav',
+                                  'resource/Female/sounds/yes.wav',
+                                  
+                                  'resource/Male/sounds/alone.wav', 'resource/Male/sounds/bored.wav',
+                                  'resource/Male/sounds/down.wav', 'resource/Male/sounds/explain.wav',
+                                  'resource/Male/sounds/get.wav', 'resource/Male/sounds/goodbye.wav',
+                                  'resource/Male/sounds/hello.wav', 'resource/Male/sounds/help.wav',
+                                  'resource/Male/sounds/howareyou.wav', 'resource/Male/sounds/hungry.wav',
+                                  'resource/Male/sounds/left.wav', 'resource/Male/sounds/move.wav',
+                                  'resource/Male/sounds/no.wav', 'resource/Male/sounds/nose.wav',
+                                  'resource/Male/sounds/pain.wav', 'resource/Male/sounds/repeat.wav',
+                                  'resource/Male/sounds/right.wav', 'resource/Male/sounds/sorry.wav',
+                                  'resource/Male/sounds/thanks.wav', 'resource/Male/sounds/thirsty.wav',
+                                  'resource/Male/sounds/up.wav', 'resource/Male/sounds/when.wav',
+                                  'resource/Male/sounds/where.wav', 'resource/Male/sounds/who.wav',
+                                  'resource/Male/sounds/yes.wav'
+                                  ]}
+                                
 setup(name='unlock', version='0.3.7', packages=packages, package_data=package_data,
       author='James Percent', author_email='james@shift5.net')
