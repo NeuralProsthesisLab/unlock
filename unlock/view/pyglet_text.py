32bcb18ece37d189414c5773d65fe1fc7976e10b
James Percent <james@syndeticlogic.net>
2013-10-24 21:09:31 -0400

    Added a HTML label wrapper.  HTML label does not work as advertised in the Pyglet documentation - with Python332 on my thinkpad.

--- unlock/view/pyglet_text.py
+++ unlock/view/pyglet_text.py
@@ -5,9 +5,33 @@ import inspect
 import logging
 import os
 
-
-class PygletTextLabel(UnlockView):
-    def __init__(self, model, canvas, text, x, y, anchor_x='center', anchor_y='center', font='Helvetica', size=48,
+class PygletLabel(UnlockView):
+    def __init__(self, model, canvas, text, x, y, color=(255,255,255,255)):
+        super(PygletLabel, self).__init__()
+        self.model = model
+        self.canvas = canvas
+        self.text = text
+        self.x = x
+        self.y = y
+        self.state = True
+        self.label = None
+        if len(color) == 3:
+            color = color + (255,)
+        self.color = color        
+        self.logger = logging.getLogger(__name__)
+        
+    def render(self):
+        self.last_state = self.state
+        self.state = self.model.get_state()
+        
+        if self.state and self.label is not None:
+            self.label.text = self.text
+        elif self.label is not None:
+            self.label.text = ''
+            
+            
+class PygletTextLabel(PygletLabel):
+    def __init__(self, model, canvas, text, x, y, width=None, height=None, anchor_x='center', anchor_y='center', font='Helvetica', size=48,
                  color=(255,255,255,255), group=None):
         """
         Draws text at a specific point on the screen
@@ -18,35 +42,35 @@ class PygletTextLabel(UnlockView):
         :param size: Size of text
         :param color: Color of text. Tuple of length four.
         """
-        self.model = model
-        self.canvas = canvas
-        self.text = text
-        self.x = x
-        self.y = y
+        super(PygletTextLabel, self).__init__(model, canvas, text, x, y, color)
         self.font = font
         self.size = size
-
-        if len(color) == 3:
-            color = color + (255,)
-        self.color = color
-        self.label = pyglet.text.Label(self.text, font_name=self.font, font_size=self.size,
-                                    x=self.canvas.x+self.x, y=self.canvas.y+self.y,
-                                    anchor_x=anchor_x, anchor_y=anchor_y, color=self.color,
-                                    group=group,batch=self.canvas.batch)
+        if width != None and height != None:
+            self.label = pyglet.text.Label(self.text, font_name=self.font, font_size=self.size,
+                                        x=self.canvas.x+self.x, y=self.canvas.y+self.y,
+                                        anchor_x=anchor_x, anchor_y=anchor_y, color=self.color, width=width, height=height,
+                                        group=group,batch=self.canvas.batch)
+        else:
+            self.label = pyglet.text.Label(self.text, font_name=self.font, font_size=self.size,
+                            x=self.canvas.x+self.x, y=self.canvas.y+self.y,
+                            anchor_x=anchor_x, anchor_y=anchor_y, color=self.color, width=width, height=height,
+                            group=group,batch=self.canvas.batch)
+            
         self.label.text = text
-        self.state = True
         self.logger = logging.getLogger(__name__)
         
-    def render(self):
-        self.last_state = self.state
-        self.state = self.model.get_state()
-        self.logger.debug("PygletTextLabel text = ", self.label.text, " last state, state = ", self.last_state, self.state)
         
-        if self.state:
-            self.label.text = self.text
-        else:
-            self.label.text = ''
-
+class PygletHTMLTextLabel(PygletLabel):
+    def __init__(self, model, canvas, text, x, y, anchor_x='center', anchor_y='center', font='Helvetica', size=18,
+                 color=(255,255,255,255), group=None):
+        super(PygletHTMLTextLabel, self).__init__(model, canvas, text, x, y, color)
+        self.label = pyglet.text.HTMLLabel(text=text,
+                                    x=self.canvas.x+self.x, y=self.canvas.y+self.y,
+                                    anchor_x=anchor_x, anchor_y=anchor_y, width=100, height=100,
+                                    group=group,batch=self.canvas.batch)
+        self.label.color = self.color
+        self.logger = logging.getLogger(__name__)
+        
 class DynamicPositionPygletTextLabel(PygletTextLabel):
     def __init__(self,  model, canvas, text, x, y, anchor_x='center', anchor_y='center', font='Helvetica', size=48,
                  color=(255,255,255,255), group=None):
@@ -67,13 +91,14 @@ class DynamicPositionPygletTextLabel(PygletTextLabel):
             
 class BellRingTextLabelDecorator(UnlockView):
     def __init__(self, text_label):
+        super(BellRingTextLabelDecorator, self).__init__()
         self.text_label = text_label
         self.model = self.text_label.model
         self.sound = pyglet.media.StaticSource(pyglet.media.load(os.path.join(os.path.dirname(inspect.getfile(BellRingTextLabelDecorator)), 'bell-ring-01.mp3')))
         self.state = True
         self.logger = logging.getLogger(__name__)        
         
-    def render(self):
+    def render(self):   
         self.last_state = self.state
         self.state = self.model.get_state()
         self.logger.debug("BellRingTextLabelDecorator text = ", self.text_label.label.text, " last state, state = ", self.last_state, self.state)
@@ -87,6 +112,4 @@ class BellRingTextLabelDecorator(UnlockView):
         else:
             self.text_label.label.text = ''
             
-
-
          
