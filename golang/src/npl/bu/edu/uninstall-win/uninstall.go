a548a8ef321df6293c941bb7193412e6c4338e06
Giang Nguyen <giang271291@gmail.com>
2013-11-04 23:34:22 +0700

    Working on installing

---
+++ golang/src/npl/bu/edu/uninstall-win/uninstall.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "os"
+    "log"
+    "path/filepath"
+)
+
+func main() {
+    conf := ParseConf(`conf.json`)
+
+    if err := os.RemoveAll(filepath.Join(conf.PythonBasePath, `Lib\site-packages\unlock`)); err != nil {
+        log.Fatalln(err)
+    }
+    
+    if err := os.Remove(filepath.Join(conf.PythonBasePath, `Lib\site-packages\unlock-0.3.7-py3.3.egg-info`)); err != nil {
+        log.Fatalln(err)
+    }
+}
