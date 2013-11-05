4c0e90488de5b189de84581d39ffb5148641d0fe
Byron Galbraith <byron.galbraith@gmail.com>
2013-11-05 17:12:53 -0500

    support for a rest period timer

--- unlock/context/test/test_context.py
+++ unlock/context/test/test_context.py
@@ -45,7 +45,7 @@ class ContextTests(unittest.TestCase):
         start = time.time()
         state, change = trial_state.update_state(0)
         self.assertEquals(unlock.util.RunState.Running, state)
-        self.assertEquals(unlock.util.TrialState.unchanged, change)
+        self.assertEquals(unlock.util.TrialState.Unchanged, change)
         self.assertEquals(1.0, trial_state.time_state.trial_duration)
         self.assertEquals(1.0, trial_state.time_state.rest_duration)                
     
@@ -60,7 +60,7 @@ class ContextTests(unittest.TestCase):
         start = time.time()
         state, change = trial_state.update_state(0)
         self.assertEquals(unlock.util.RunState.Running, state)
-        self.assertEquals(unlock.util.TrialState.unchanged, change)
+        self.assertEquals(unlock.util.TrialState.Unchanged, change)
         self.assertEquals(1.0, trial_state.time_state.trial_duration)
         self.assertEquals(1.0, trial_state.time_state.rest_duration)        
             
