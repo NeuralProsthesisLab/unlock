4d12f444ff4fb5000f9a1b439daaf3ac0595c5db
Giang Nguyen <giang271291@gmail.com>
2013-11-06 18:06:54 +0700

    Use direct argument for hash.go

--- golang/src/npl/bu/edu/hash/hash-packages.bat
+++ golang/src/npl/bu/edu/hash/hash-packages.bat
@@ -1,13 +1,13 @@
-go run hash.go -file C:\Users\User\Documents\unlock\package\python-3.3.2.msi
-go run hash.go -file C:\Users\User\Documents\unlock\package\vcredist_2010_x86.exe
-go run hash.go -file C:\Users\User\Documents\unlock\package\numpy-MKL-1.7.1.win32-py3.3.exe
-go run hash.go -file C:\Users\User\Documents\unlock\package\AVbin10-win32-x86.exe
-go run hash.go -file C:\Users\User\Documents\unlock\package\pyglet-1.2alpha-p3.zip
-go run hash.go -file C:\Users\User\Documents\unlock\package\pyserial-2.6.zip
-go run hash.go -file C:\Users\User\Documents\unlock\package\pyaudio-0.2.7.py33.exe
-go run hash.go -file C:\Users\User\Documents\unlock\package\pywin32-218.win32-py3.3.exe
-go run hash.go -file C:\Users\User\Documents\unlock\package\unlock-0.3.7-win32.zip
-go run hash.go -file C:\Users\User\Documents\unlock\package\unlock-x86.exe
-go run hash.go -file C:\Users\User\Documents\unlock\package\unlock-x86-64.exe
-go run hash.go -file C:\Users\User\Documents\unlock\package\uninstall-win.exe
-go run hash.go -file C:\Users\User\Documents\unlock\package\uninstall.bat
+go run hash.go C:\Users\User\Documents\unlock\package\python-3.3.2.msi
+go run hash.go C:\Users\User\Documents\unlock\package\vcredist_2010_x86.exe
+go run hash.go C:\Users\User\Documents\unlock\package\numpy-MKL-1.7.1.win32-py3.3.exe
+go run hash.go C:\Users\User\Documents\unlock\package\AVbin10-win32-x86.exe
+go run hash.go C:\Users\User\Documents\unlock\package\pyglet-1.2alpha-p3.zip
+go run hash.go C:\Users\User\Documents\unlock\package\pyserial-2.6.zip
+go run hash.go C:\Users\User\Documents\unlock\package\pyaudio-0.2.7.py33.exe
+go run hash.go C:\Users\User\Documents\unlock\package\pywin32-218.win32-py3.3.exe
+go run hash.go C:\Users\User\Documents\unlock\package\unlock-0.3.7-win32.zip
+go run hash.go C:\Users\User\Documents\unlock\package\unlock-x86.exe
+go run hash.go C:\Users\User\Documents\unlock\package\unlock-x86-64.exe
+go run hash.go C:\Users\User\Documents\unlock\package\uninstall-win.exe
+go run hash.go C:\Users\User\Documents\unlock\package\uninstall.bat
