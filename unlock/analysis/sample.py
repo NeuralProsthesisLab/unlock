bc1bf9b630022470123229f37ccfa60451f0853c
Giang Nguyen <giang271291@gmail.com>
2013-11-19 02:23:08 +0700

    Added sample linear regression plot

--- unlock/analysis/sample.py
+++ unlock/analysis/sample.py
@@ -1,11 +1,30 @@
 import argparse
+import os
+import numpy
+from matplotlib.pyplot import *
 
 if __name__ == '__main__':
     argParser = argparse.ArgumentParser()
     argParser.add_argument('inputFile')
-    argParser.add_argument('-export', dest='exportPath')
+    argParser.add_argument('-export', dest='exportPath', default=False)
     args = argParser.parse_args()
     
-    fout = open(args.exportPath, 'wb')
-    fout.write(b'blah')
-    fout.close()
+    fin = open(args.inputFile, 'r')
+    t,y = [],[]
+    next(fin) # Ignore first line (which is usually column names) in csv
+    for line in fin:
+        pair = line.split(',')
+        t.append(float(pair[1]))
+        y.append(float(pair[2]))
+    fin.close()
+    
+    a,b = numpy.polyfit(t,y,1)
+    approximate = [a*t1 + b for t1 in t]
+    
+    plot(t,y,'.')
+    plot(t,approximate)
+    
+    if (args.exportPath):
+        savefig(args.exportPath)
+    else:
+        show()
