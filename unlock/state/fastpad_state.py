bd1c2af8d8464012e75f5ceb3057a677651c1f9b
Karl Wiegand <wiegand@ccs.neu.edu>
2014-03-11 17:57:50 -0400

    - Working on new FastPad state machine

--- unlock/state/fastpad_state.py
+++ unlock/state/fastpad_state.py
@@ -1,78 +1,100 @@
+# Created By: Karl Wiegand (wiegand@ccs.neu.edu)
+# Date Created: Tue Mar 11 16:29:35 EDT 2014
+# Description: FastPad state and behavior
+
+# General libs
+from enum import Enum
+
+# Unlock libs
 from unlock.state.state import UnlockState
 
 class FastPadState(UnlockState):
+    """Text composition on a telephone number pad."""
+
+    # Constants: directional commands
     UP = 1
     DOWN = 2
     LEFT = 3
     RIGHT = 4
-    SELECT_TIME = 2
+
+    # Constants: state management
+    BUTTON = 1 # Cursor is inside of a button
+    PAD = 2 # Cursor is a button on the pad
+    VOICE = 3 # System is speaking
+
+    # Constants: logical form of rectangular number
+    # pad as lists of case-insensitive strings
+    # (Note: referenced as list indices with [0, 0]
+    # referring to the upper-leftmost list)
+    NUMPAD = [
+            [[], ["A", "B", "C"], ["D", "E", "F"]],
+            [["G", "H", "I"], ["J", "K", "L"], ["M", "N", "O"]],
+            [["P", "Q", "R", "S"], ["T", "U", "V"], ["W", "X", "Y", "Z"]],
+            [["<"], [" "], [".", "?", "!"]],
+            ]
+
+    # Constants: strings that have special meanings
+    ERASE = {"<"} # Remove the last string entry from the current text
+    SPEAK = {".", "?", "!"} # Speak the current text
+
     def __init__(self):
-       # Initialize the state
+        """Initialize the state."""
         super(FastPadState, self).__init__()
-        self.previous_mode = "CURSOR"
-        self.mode = "CURSOR"
-        self.currButton = None
-        self.button = None
-        self.selTime = 0
-        self.noop = False
+
+        # Start in the center of the pad
+        self.state = FastPadState.BUTTON
+        self.button = [1, 1] # Center of NUMPAD
+        self.item = None # Integer list position (int) or None
+        self.text = [] # List of added strings from NUMPAD
             
     def process_command(self, command):
-        """
-        Update the screen; called periodically on refresh.
+        """Update the screen; called periodically on refresh.
 
-        Input:
-            timeDelta -- (float) Number of seconds since the
-                previous call to update()
-            decision -- (int) Decision, if any; one of UP, DOWN,
-                LEFT, or RIGHT
-            selection -- (int) 1 if a selection was made
-
-        Raises an Exception if anything goes wrong.
+        Args:
+            command: (object?) The user's BCI or keyboard command
         """
-        print("fastpad_state.ln 32 KARL I GOT A COMMAND")
-        self.noop = False
+        # Cache our previous state
+        oldState = self.state
+        oldButton = self.button
+        oldItem = self.item
+
+        # Calculate our new state
         if command.decision == FastPadState.LEFT:
-            
-            self.mode = "CURSOR"
-            self.button = self.currButton.left
+
+            # Make the move
+            self.state = FastPadState.BUTTON
+            self.button[1] -= 1
+            self.item = None
+
+            # Wrap the board
+            if self.button[1] < 0:
+                self.button[1] = len(NUMPAD[self.button[0]])
                     
         elif command.decision == FastPadState.RIGHT:
-            self.mode = "CURSOR"
-            self.button = self.currButton.right
+
+            # Make the move
+            self.state = FastPadState.BUTTON
+            self.button[1] += 1
+            self.item = None
+
+            # Wrap the board
+            if self.button[1] > len(NUMPAD[self.button[0]])
+                self.button[1] = 0
             
         elif command.decision == FastPadState.UP:
-            self.mode = "CURSOR"
-            self.button = self.currButton.up
+            
+            # Make the move
+            self.state = FastPadState.BUTTON
+            self.button[0] -= 1
+            self.item = None
+
+            # Wrap the board
+            if 
             
         elif command.decision == FastPadState.DOWN:
-            self.mode = "CURSOR"
-            self.button = self.currButton.down
+
+            pass
             
         elif command.selection:
-            self.mode = "SELECT"
-            self.button = self.currButton
-            # We've changed our selection, so reset the timer
-            self.selTime = 0
             
-        else:
-            # If we're in selection mode, track the time
-            if self.mode == "SELECT":
-                
-                # Add the time
-                self.selTime += command.delta
-                
-                # Should we select self item?
-                if self.selTime >= FastPadState.SELECT_TIME:
-                    
-                    self.selTime = 0
-                    self.mode = "CURSOR"
-                    self.button = self.currButton
-                else:
-                    self.noop = True
-                    
-            # If we're not in selection mode, reset the timer
-            else:
-                self.selTime = 0
-                self.noop = True
-
-                
+            pass 
