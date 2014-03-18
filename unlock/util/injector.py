c04e71f1ea22bf289bd9fa9941dff15fe1fca242
James Percent <james@syndeticlogic.net>
2014-03-18 01:22:52 -0400

    Refactored the dependency injection code from UnlockRuntime into a separate file.  This is because it will be used in 2 places now.

---
+++ unlock/util/injector.py
@@ -0,0 +1,73 @@
+# Copyright (c) James Percent and Unlock contributors.
+# All rights reserved.
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#    1. Redistributions of source code must retain the above copyright notice,
+#       this list of conditions and the following disclaimer.
+#
+#    2. Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#    3. Neither the name of Unlock nor the names of its contributors may be used
+#       to endorse or promote products derived from this software without
+#       specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.import socket
+
+__author__ = 'jpercent'
+
+class DependencyInjector(object):
+    def __init__(self, factory=None):
+        super(DependencyInjector, self).__init__()
+        self.factory = factory
+
+    def configure_application(self, config, factory=None):
+        assert (factory or self.factory) and config
+
+        if not factory:
+            factory = self.factory
+
+        DependencyInjector.configure_singletons(config, factory)
+        instance = None
+        for key, value in config.items():
+            if 'main' in value:
+                instance = factory.create(key, config)
+                break
+        assert instance
+        return instance
+
+    @staticmethod
+    def configure_singletons(config, factory):
+        level = 0
+        max_level = 0
+        while level <= max_level:
+            pop_keys = set([])
+            for key, value in config.items():
+                if not 'singleton' in value:
+                    continue
+
+                level_value = value['singleton']
+                if level_value > max_level:
+                    max_level = level_value
+
+                if level_value <= level:
+                    #print(key, value)
+                    assert 'name' in value
+                    factory.create_singleton(value['name'], key, config)
+                    pop_keys.add(key)
+
+            for key in pop_keys:
+                config.pop(key)
+            level += 1
+
