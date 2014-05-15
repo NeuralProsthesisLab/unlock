b31b375154462faacc55e14de3f224f91af6d9c3
Byron Galbraith <byron.galbraith@gmail.com>
2014-05-15 00:30:07 -0400

    removed non-checkerboard-specific properties

--- unlock/view/checkerboard_sprite.py
+++ unlock/view/checkerboard_sprite.py
@@ -71,10 +71,6 @@ class CheckerboardProperties(object):
 
     :param width: width of the checkerboard in pixels
     :param height: height of the checkerboard in pixels
-    :param anchor: anchor position on the screen (SpritePositionComputer)
-    :param rotation: degrees rotated counterclockwise
-    :param x_offset: offset from anchor position in pixels
-    :param y_offset: offset from anchor position in pixels
     :param x_checks: total number of horizontal checks
     :param y_checks: total number of vertical checks
     :param x_ratio: width ratio between alternating checks
@@ -82,14 +78,11 @@ class CheckerboardProperties(object):
     :param color1: RGB color of one set of checks
     :param color2: RGB color of the other set of checks
     """
-    def __init__(self, width=300, height=300, anchor=None, rotation=0,
-                 x_offset=0, y_offset=0, x_checks=4, y_checks=4, x_ratio=1,
-                 y_ratio=1, color1=(0, 0, 0), color2=(255, 255, 255)):
+    def __init__(self, width=300, height=300, x_checks=4, y_checks=4,
+                 x_ratio=1, y_ratio=1, color1=(0, 0, 0),
+                 color2=(255, 255, 255)):
         self.width = width
         self.height = height
-        self.rotation = rotation
-        self.x_offset = x_offset
-        self.y_offset = y_offset
         self.x_checks = x_checks
         self.y_checks = y_checks
         self.x_ratio = x_ratio
