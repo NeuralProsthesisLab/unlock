e228563aa52638d72d4579d8d93b2f78eaad0a15
Byron Galbraith <byron.galbraith@gmail.com>
2014-10-08 17:13:44 -0400

    send wheel control commands on decision/selection

--- unlock/state/robot_state.py
+++ unlock/state/robot_state.py
@@ -1,4 +1,5 @@
 import io
+import json
 
 import pyglet
 
@@ -7,12 +8,31 @@ from unlock.util.streamclient import StreamClient
 
 
 class RobotManualDriveState(UnlockState):
+    SERVO_SPEED = 0.175
+    WHEEL_SPEED = 50
+
     def __init__(self):
         super(RobotManualDriveState, self).__init__()
         self.sc = StreamClient('128.197.61.111', 21567)
         self.frame = None
 
     def process_command(self, command):
+        cmds = {}
+        if command.decision == 1:
+            cmds['wheels'] = [self.WHEEL_SPEED, self.WHEEL_SPEED]
+        elif command.decision == 2:
+            cmds['wheels'] = [-self.WHEEL_SPEED, -self.WHEEL_SPEED]
+        elif command.decision == 3:
+            cmds['wheels'] = [self.WHEEL_SPEED, -self.WHEEL_SPEED]
+        elif command.decision == 4:
+            cmds['wheels'] = [-self.WHEEL_SPEED, self.WHEEL_SPEED]
+
+        if command.selection:
+            cmds['wheels'] = [0, 0]
+
+        if len(cmds) > 0:
+            self.sc.set('bri/command', json.dumps(cmds))
+
         try:
             data = self.sc.get('bri/create/video.jpg')
             buffer = io.BytesIO(data)
