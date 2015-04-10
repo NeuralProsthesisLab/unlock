bcd303341730a194675c5c1db0e98967e7104a81
bgalbraith <byron.galbraith@gmail.com>
2015-04-10 17:29:13 -0400

    own and handle stimuli rendering

--- unlock/view/experiment_view.py
+++ unlock/view/experiment_view.py
@@ -1,12 +1,13 @@
 from unlock.view import UnlockView, PygletTextLabel
 from unlock.state import UnlockState
 
-
 class ExperimentView(UnlockView):
-    def __init__(self, model, canvas):
+    def __init__(self, model, canvas, normal_view, overlap_view):
         super(ExperimentView, self).__init__()
         self.model = model
         self.canvas = canvas
+        self.normal_view = normal_view
+        self.overlap_view = overlap_view
         cx, cy = canvas.center()
         self.text = PygletTextLabel(UnlockState(True), canvas, '', cx, cy)
 
@@ -15,3 +16,9 @@ class ExperimentView(UnlockView):
         if state is not None:
             self.text.label.text = state.label
             self.text.label.font_size = state.size
+            color = getattr(state, 'color', (255, 255, 255, 255))
+            self.text.label.color = color
+        for view in self.overlap_view:
+            view.render()
+        for view in self.normal_view:
+            view.render()
